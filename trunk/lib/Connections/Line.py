8702a21977c0fd06aec60a146370133be68efd04
miroslav <miroslav@552e5214-defe-4a50-a20b-3e92271688c2>
2006-06-22 11:21:49 +0000

    Created by Miroslav <miroslav.spigura@gmail.com>

--- trunk/lib/Connections/Line.py
+++ trunk/lib/Connections/Line.py
@@ -1,24 +1,41 @@
-class CConnectionLine:
-    def __init__(self):
-        pass
+import gtk.gdk
+
+LINE_STYLES = {'solid': gtk.gdk.LINE_SOLID,
+               'dot': gtk.gdk.LINE_ON_OFF_DASH,
+               'doubledot': gtk.gdk.LINE_DOUBLE_DASH}
+
+
+class CConnectionLine(object):
+    def __init__(self, color = 'black', style = 'solid', width = 1):
+        self.color = gtk.gdk.color_parse(color)
+        self.style = style
+        self.width = int(width)
 
     def GetColor(self):
-        pass
+        return self.color
 
     def GetStyle(self):
-        pass
+        return self.style
 
     def GetWidth(self):
-        pass
+        return self.width
 
-    def Paint(self, x1, y1, x2, y2, line):
-        pass
+    def Paint(self, x1, y1, x2, y2, Connection):
+        wgt = Connection.GetDrawingArea().GetDrawable()
+        cmap = wgt.get_colormap()
+        gc = wgt.new_gc(foreground = cmap.alloc_color(self.color),  line_width = self.width, line_style = LINE_STYLES[self.style])
+        wgt.draw_line(gc, x1, y1, x2, y2)
+        
 
     def SetColor(self, value):
-        pass
+        self.color = value
 
     def SetStyle(self, value):
-        pass
+        self.style = value
 
     def SetWidth(self, value):
-        pass
+        self.width = value 
+    
+    Color = property(GetColor, SetColor)
+    Style = property(GetStyle, SetStyle)
+    Width = property(GetWidth, SetWidth)
--- trunk/lib/Projekt/ProjectNode.py
+++ trunk/lib/Projekt/ProjectNode.py
@@ -1,24 +1,64 @@
-class CProjectNode:
-    def __init__(self, parent):
-        pass
+from lib.lib import UMLException
 
+class CProjectNode(object):
+    def __init__(self, parent = None, type = None):
+        self.parent = parent
+        self.childs = []
+        self.drawingareas = []        
+        self.type = type
+    
+    def GetType(self):
+        return self.type
+    
+    def GetName(self):
+        return self.type.GetName()
+    
     def AddChild(self, child):
-        pass
+        if child not in self.childs:
+            self.childs.append(child)
+            child.parent = self
+        else:
+            raise UMLException("ExistsChild")
+            
 
     def AddDrawingArea(self, area):
-        pass
-
-    def GetChild(self, index):
-        pass
-
+        if area not in self.drawingareas:
+            self.drawingareas.append(area)
+        else:
+            raise UMLException("ExistsArea")
+    
+    def GetChild(self, name):
+        for i in childs:
+            if i.GetName() == name:
+                return i
+        else:
+            return None
+    
+    def GetIndexChild(self, index):
+        if index <= len(self.childs) - 1:
+            return self.childs[index]
+        else:
+            raise UMLException("NodeNotExists")
+    
+    def GetChilds(self):
+        return self.childs
+    
     def GetParent(self):
-        pass
+        return self.parent
 
     def RemoveChild(self, child):
-        pass
-
+        if child in self.childs: 
+            self.childs.remove(child)
+        else:
+            raise UMLException("ChildNotExists")
+    
     def RemoveDrawingArea(self, area):
-        pass
+        if area in self.drawingareas:
+            self.drawingareas.remove(area)
+        else:
+            raise UMLException("AreaNotExists")
 
     def SetParent(self, parent):
-        pass
+        self.parent = parent
+        
+    Parent = property(GetParent,SetParent)
