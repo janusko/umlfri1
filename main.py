2cee30218644f5972860611afaa14ef65acecc46
Pavol Kovalik <kovalikp@users.noreply.github.com>
2006-06-22 11:06:53 +0000

    Created by Palino <pavol.kovalik@st.fri.utc.sk>

--- main.py
+++ main.py
@@ -4,54 +4,26 @@ import pygtk
 
 pygtk.require('2.0')
 
-import gtk, gtk.glade
+import lib.Gui.common
+import os.path
 
-class MainWindow:
-    def __init__(self, app):
-        self.w_tree = gtk.glade.XML('gui/gui.glade')
-        self.w_tree.signal_autoconnect(self)
-        
-        self.app = app
-        self.window = self.w_tree.get_widget('frmMain')
-        self.tbTools = self.w_tree.get_widget('tbTools')
-        self.hboxWorkSpace = self.w_tree.get_widget('hboxWorkSpace')
-        self.frmAbout = self.w_tree.get_widget('frmAbout')
-        self.frmAbout_cmdOkay = self.w_tree.get_widget('cmdOkay')
-        
-        self.w_tree.get_widget('mnuUseCaseDiagram').set_sensitive(False)
-    
-    def on_frmMain_destroy(self, frm):
-        self.app.quit()
-    
-    def on_mnuQuit_activate(self, mnu):
-        self.app.quit()
-    
-    def on_mnuViewTools_activate(self, mnu):
-        # self.tbTools.set_child_visible(mnu.get_active())
-        if mnu.get_active():
-            self.hboxWorkSpace.pack_start(self.tbTools, expand=False, fill=False)
-            self.hboxWorkSpace.reorder_child(self.tbTools, 0)
-        else:
-            self.hboxWorkSpace.remove(self.tbTools)
-    
-    def on_mnuAbout_activate(self, mnu):
-        self.frmAbout.set_property('visible', True)
-    
-    def on_frmAbout_delete_event(self, win, event):
-        win.set_property('visible', False)
-        return True
-    
-    def on_cmdOkay_clicked(self, btn):
-        self.frmAbout.set_property('visible', False)
+from lib.Gui import CfrmMain, CfrmAbout, CfrmProperties, CfrmAttribute, CfrmOperation
+from lib.Diagrams import CDiagramFactory
+from lib.Elements import CElementFactory
+from lib.Connections import CConnectionFactory
 
-class MainApp:
+from lib.consts import DIAGRAMS_PATH, ELEMENTS_PATH, CONNECTIONS_PATH
+
+class Application(lib.Gui.common.CApplication):
+    windows = (CfrmMain, CfrmAbout, CfrmProperties, CfrmAttribute, CfrmOperation)
+    glade = os.path.join(os.path.dirname(__file__), 'gui/gui.glade')
+    main_window = 'frmMain'
+    
     def __init__(self):
-        self.main_window = MainWindow(self)
-    
-    def main(self):
-        gtk.main()
-    
-    def quit(self):
-        gtk.main_quit()
+        self.DiagramFactory = CDiagramFactory(DIAGRAMS_PATH)
+        self.ElementFactory = CElementFactory(ELEMENTS_PATH)
+        self.ConnectionFactory = CConnectionFactory(CONNECTIONS_PATH)
+        lib.Gui.common.CApplication.__init__(self)
+        self.Project = None
 
-MainApp().main()
+Application().Main()
