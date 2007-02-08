0195effd11b7f049ab9fa6fd75ec8cdacf044cd0
janik <jan.janech@kst.uniza.sk>
2007-02-08 16:20:02 +0000

    - Updated project logo and program icon - Created window for exceptions

--- lib/debug.py
+++ lib/debug.py
@@ -1,4 +1,8 @@
 import sys
+import traceback
+
+import gtk
+import pango
 
 class DebugAttribute(object):
     def __init__(self, name, val = None):
@@ -20,3 +24,42 @@ class DebugAttribute(object):
         print 'Get %s.%s (%r)'%(instance.__class__.__name__, self.name, self.val)
         print '========================'
         return self.val
+
+def display_exc():
+    win = gtk.Window()
+    win.set_title('Exception')
+    text = gtk.TextView()
+    text.set_editable(False)
+    buffer = text.get_buffer()
+    text.show()
+    win.add(text)
+    win.show()
+    
+    buffer.create_tag("italic", style=pango.STYLE_ITALIC)
+    buffer.create_tag("bold", weight=pango.WEIGHT_BOLD)
+    buffer.create_tag("monospace", family="monospace")
+    
+    iter = buffer.get_iter_at_offset(0)
+    
+    buffer.insert_with_tags_by_name(iter, "Traceback (most recent call last):\n", "bold")
+    
+    exccls, exc, tb = sys.exc_info()
+    for filename, line, function, text in traceback.extract_tb(tb)[1:]:
+        buffer.insert(iter, "\tFile \"")
+        buffer.insert_with_tags_by_name(iter, filename, "bold")
+        buffer.insert(iter, "\", line ")
+        buffer.insert_with_tags_by_name(iter, str(line), "bold")
+        if function is not None and function != '?':
+            buffer.insert(iter, ", in ")
+            buffer.insert_with_tags_by_name(iter, str(function), "bold")
+        buffer.insert(iter, "\n")
+        if text is not None:
+            buffer.insert(iter, "\t\t")
+            buffer.insert_with_tags_by_name(iter, str(text), "monospace")
+            buffer.insert(iter, "\n")
+    
+    buffer.insert(iter, "\n")
+    buffer.insert_with_tags_by_name(iter, exccls.__name__, "bold")
+    buffer.insert(iter, ": %s\n"%exc)
+    
+    traceback.print_exc()
