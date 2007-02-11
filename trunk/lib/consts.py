d1e005e20f6c9b89c1ba0d8c0c7d89aa3f0a8ec1
palino <palino@552e5214-defe-4a50-a20b-3e92271688c2>
2007-02-11 18:34:07 +0000

    handy scrolling when space is pressed

--- trunk/lib/consts.py
+++ trunk/lib/consts.py
@@ -13,6 +13,8 @@ ICONS_PATH = 'icons'
 
 ARROW_IMAGE = 'arrow.png'
 DEFAULT_TEMPLATE_ICON = 'default_icon.png'
+GRAB_CURSOR = 'grab.png'
+GRABBING_CURSOR = 'grabbing.png'
 
 PROJECT_EXTENSION = '.frip'
 PROJECT_TPL_EXTENSION = '.frit'
