ad465f683dd5ab0096dfd88404a62cb924de35e0
blueway <blueway@552e5214-defe-4a50-a20b-3e92271688c2>
2008-07-24 09:54:23 +0000

    iterchildren to getchildren

--- trunk/lib/Drawing/Objects/Svg.py
+++ trunk/lib/Drawing/Objects/Svg.py
@@ -40,7 +40,7 @@ class CSvg(CVisualObject):
     
     def LoadXml(self, element):
         def recursive(parent, transform):
-            for node in parent.iterchildren():
+            for node in parent.getchildren():
                 attrs = self.__getattrs(node)
                 if 'transform' in attrs:
                     transform = transform*self.__parsetransform(attrs['transform'])
