e3d8c2948596b11c52a24af0a0496014d624e5f2
janik <jan.janech@kst.uniza.sk>
2008-07-31 19:18:24 +0000

    New templates

--- share/locale/update_catalog.sh
+++ share/locale/update_catalog.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-find ../.. | grep -E "(glade|py)$" | grep -v setup.py | xgettext -f- -o uml_fri.pot
+PACKAGE_NAME='UML .FRI'
+PACKAGE_VERSION="$(grep __version__ ../../main.py | cut -d"'" -f2)"
+
+find ../.. | grep -E "(glade|py)$" | grep -v setup.py | xgettext -f- -o uml_fri.pot -s --no-location --package-name="$PACKAGE_NAME" --package-version="$PACKAGE_VERSION"
