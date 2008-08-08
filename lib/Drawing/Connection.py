2e3cc2295e1e92232f271d8c2d9d0a8ff5772d69
tomas <tomas.fri.zu@gmail.com>
2008-08-08 14:02:08 +0000

    BUG #0000024 solved, included: - added /Styles/Connection/ArrowAngleSteps to config.xml + change to config.xsd - roudning of anlge algorithm added to CArrow.Paint method
    Sematic error corrected in geometry.py
    
    Improved algorithm for validating points in CConnection.ValidatePoints. included:
    - added /Styles/Connection/MinimalAngle to config.xml + change to config.xsd
    - new algorithm in CConnection.ValidPoint, used in CConection.ValidatePoints
    
    A lot of new docstrings in CConection.
    Attention: some of them are already written for new version of this class and are not 100% consistent with this version.

--- lib/Drawing/Connection.py
+++ lib/Drawing/Connection.py
@@ -5,7 +5,59 @@ from lib.Math2D import CPoint, CLine, CLineVector, CPolyLine, CRectangle
 from math import sqrt, atan2, pi
 
 class CConnection:
+    '''Graphical representation of connection
+    
+    In the program you have to distinguish between logical connection and its
+    graphical representation.
+    Logical connection between source and destination is only one in a project,
+    but it can have several graphical representations, in each diagram one.
+    
+    @note: in text below, if not written otherwise, "connection" means its
+    graphical representation
+    
+    @ivar diagram: owner of connection
+    @type diagram: L{CDiagram<Diagram.CDiagram>}
+    
+    @ivar object: reference to logical connection
+    @type object: L{CConnectionObject<CConnectionObject>}
+    
+    @ivar points: list of (x, y) positions of points forming poly line
+    @type points: list
+    
+    @ivar selpoints: index of selected point (to be moved), None if any is not 
+    selected
+    @type selpoints: int / NoneType
+    
+    @ivar labels: dictionary of pairs {id: L{label<CConLabelInfo>}}
+    @type labels: dict
+    
+    @ivar source: Element at the beginning of the connection
+    @type source: L{CElement<CElement>}
+    
+    @ivar destination: Element at the end of the connection
+    @type destination: L{CElement<CElement>}
+    '''
+    
     def __init__(self, diagram, obj, source, destination, points = None):
+        '''Create new instance of connection
+        
+        @param diagram: owner of the connection
+        @type  diagram: L{CDiagram<Diagram.CDiagram>}
+        
+        @param obj: logical connection between source and destination
+        @type  obj:
+        
+        @param source: Element at the beginning of the connection
+        @type  source: L{CElement<CElement>}
+        
+        @param destination: Element at the end of the connection
+        @type  destination: L{CElement<CElement>}
+        
+        @param points: list of points [(x1, y1), (x2, y2), ... ] forming the
+        connection line
+        @type  points: list
+        '''
+        
         self.diagram = diagram
         self.diagram.AddConnection(self)
         self.object = obj
@@ -54,18 +106,40 @@ class CConnection:
         return self.selected
         
     def SelectPoint(self, index):
+        '''set self.selpoint to index if index within range
+        
+        @param index: index of point to be selected
+        @type  index: int
+        '''
         if 0 < index <= len(self.points):
             self.selpoint = index
         else:
             raise ConnectionError("PointNotExists")
             
     def DeselectPoint(self):
+        '''set self.selpoint to None'''
         self.selpoint = None
         
     def GetSelectedPoint(self):
+        '''
+        Get index of selected point. None if no one is selected.
+        
+        @return: self.selpoint
+        @rtype: int / NoneType
+        '''
         return self.selpoint
     
     def GetPointAtPosition(self, pos):
+        '''
+        Get index of point from connection, if there is one close enough to 
+        point defined by pos. None if there is no close point.
+        
+        @param pos: (x, y) position
+        @type  pos: tuple
+        
+        @return: index of point or None
+        @rtype: int / NoneType
+        '''
         x, y = pos
         size = config['/Styles/Selection/PointsSize']
         for i, point in enumerate(self.points):
@@ -75,21 +149,67 @@ class CConnection:
             return None
             
     def GetSquare(self, canvas):
+        '''get absolute positoin of minimal rectangle to which line fits,
+        excluding labels.
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        
+        @return: ((left, top), (right, bottom))
+        @rtype: tuple
+        '''
         left, top, right, bottom = 1000000, 1000000, -1000000, -1000000
         for x, y in self.GetPoints(canvas):
             left, top, right, bottom = min(left, x), min(top, y), max(right, x), max(bottom, x)
         return ((left, top), (right, bottom))
         
     def GetSource(self):
+        '''
+        Get element at the beginning of connection
+        
+        @return: self.source
+        @rtype: L{CElement<CElement>}
+        '''
         return self.source
         
     def GetDestination(self):
+        '''
+        Get element at the end of connection
+        
+        @return: self.destination
+        @rtype: L{CElement<CElement>}
+        '''
         return self.destination
         
     def GetSourceObject(self):
+        """
+        Get source object of logical connection
+        
+        @return: connection source
+        @rtype:  L{CElementObject<CElementObject>}
+        """
         return self.object.GetSource()
         
+    def GetDestinationObject(self):
+        """
+        Get destination object of logical connection
+        
+        @return: connection destination
+        @rtype:  L{CElementObject<CElementObject>}
+        """
+        return self.object.GetDestination()
+        
     def GetNeighbours(self, index, canvas):
+        '''get positions of neighbouring points to point 
+        selected by index.
+        
+        @attention: Indexing starts with 1 (in this case).
+
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+
+        @return: ((x1,y1),(x2,y2)) 
+        '''
         if not (0 < index  <= len(self.points)):
             raise ConnectionError("PointNotExists")
         if index == 1:
@@ -109,13 +229,34 @@ class CConnection:
         newline = CLineVector(scaled.GetEnd(), angle + lineang,  dist)
         return newline.GetEnd().GetPos()
         
-    def GetDestinationObject(self):
-        return self.object.GetDestination()
-        
     def GetObject(self):
+        '''Get object of logical connection
+        
+        @return: logical connection
+        @rtype: L{CConnectionObject<CConnectionObject>}
+        '''
         return self.object
     
     def GetLabelPosition(self, canvas, id, position, size):
+        '''
+        Get absolute (x,y) position of label defined by id
+        
+        If connection doesn't have id in cache, it saves it and writes
+        it's position. Position is calculated from position and size so that
+        center of label is at the default position.
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        
+        @param id: identifier of label
+        @type  id: whatever hasheable
+        
+        @param position: default position of label - valid value
+        @type  position: str
+        
+        @param size: logical size of label (width, height)
+        @type  size: tuple
+        '''
         width, height = size
         if id in self.labels:
             pnt, idx, t, dist, angle = self.labels[id]
@@ -203,6 +344,30 @@ class CConnection:
         self.labels[label] = [pos, index, t, dist, angle]
         
     def InsertPoint(self, canvas, point, index = None):
+        '''
+        Add new point forming polyline of connection
+        
+        Label can be moved, if new point appears at the same segment of 
+        polyline to which is this label bound. At first, relative position
+        is adjusted to new situation - label.pos, then absolute position is 
+        recalculated and again relative position is recalculated to make sure,
+        that label is bound to closest segment of polyline.
+        
+        Creation of new point is ignored if new point is too close to 
+        neighbouring point or angle the two new segments form is too close to
+        pi. 
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        
+        @param point: (x, y) position of point to be appended
+        @type  point: tuple
+        
+        @param index: position at polyline to which to put new point. 
+        @type  index: int
+        
+        @raise IndexError: if 0 > index or len(self.points) < index
+        '''
         if index is None:
             self.points.append(point)
             return
@@ -230,9 +395,30 @@ class CConnection:
         self.ValidatePoints(canvas)
     
     def AddPoint(self, point):
+        '''
+        Append next point forming polyline as last
+        
+        @attention: use only during loading project from file, as no 
+        calculations are performed 
+        
+        @param point: point to be appended (x, y)
+        @type  point: tuple
+        '''
         self.points.append(point)
         
     def WhatPartOfYouIsAtPosition(self, canvas, point):
+        '''
+        What is on the position defined by point
+        
+            - L{CConLabelInfo<CConLabelInfo>} instance
+            - index of line, forming connection
+            - None, if not hit
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+
+        @rtype: L{CConLabelInfo<CConLabelInfo>} / int / NoneType
+        '''
         points = list(self.GetPoints(canvas))
         point = CPoint(point)
         point1 = points[0]
@@ -245,9 +431,24 @@ class CConnection:
             return None
     
     def AreYouAtPosition(self, canvas, point):
+        '''
+        Get state whether point hits a part of connection, labels including
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+
+        @return: True if L{WhatPartOfYouIsAtPosition
+        <self.WhatPartOfYouIsAtPosition>} returns something
+        @rtype: bool
+        '''
         return self.WhatPartOfYouIsAtPosition(canvas, point) is not None
 
     def MoveAll(self, delta):
+        '''Move all points and labels of connection
+        
+        @param delta: (dx, dy) distance to move
+        @type  delta: tuple
+        '''
         deltax, deltay = delta
         points = []
         for x, y in self.points:
@@ -259,6 +460,20 @@ class CConnection:
         self.points = points
         
     def MovePoint(self, canvas, point, index):
+        '''
+        Change position of point defined by index to to new position pos
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        
+        @param pos: (x, y) new position of point
+        @type  pos: tuple
+        
+        @param index: index of point in self.points
+        @type  index: int
+        
+        @raise IndexError: if index <= 0 or index > len(self.points)
+        '''
         if 0 < index <= len(self.points):
             self.points[index - 1] = point
             for id in self.labels:
@@ -270,6 +485,20 @@ class CConnection:
         self.ValidatePoints(canvas)
 
     def Paint(self, canvas, delta = (0, 0)):
+        '''
+        Paint connection including labels at canvas
+        
+        In fact L{CConnectionObject.Paint<CConnectionObject.Paint>} is used to 
+        paint polyline itself. This method is afterwards responsible for
+        drawing rectangles if the connection is selected.
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        
+        @param delta: (x, y) offset by which is drawing area moved by 
+        scrollbars on the screen
+        @type  delta: tuple
+        '''
         size = config['/Styles/Selection/PointsSize']
         self.object.Paint(canvas, self, delta)
         if self.selected is True:
@@ -278,6 +507,18 @@ class CConnection:
                 canvas.DrawRectangle((i[0] + dx - size//2, i[1] + dy - size//2), (size, size), config['/Styles/Selection/PointsColor'])
 
     def RemovePoint(self, canvas, index):
+        '''
+        Delete point from polyline and colapse two neighbouring segments of 
+        polyline
+        
+        @param index: index of point to be deleted
+        @type  index: int
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+
+        @raise IndexError: if index < 0 or index > len(self.points)
+        '''
         if 0 < index <= len(self.points):
             prevpoint = self.GetPoint(canvas, index - 1)
             point = self.GetPoint(canvas, index )
@@ -304,6 +545,11 @@ class CConnection:
         self.ValidatePoints(canvas)
     
     def GetPoints(self, canvas):
+        '''
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        '''
         yield self.GetPoint(canvas, 0)
             
         for point in self.points:
@@ -312,6 +558,11 @@ class CConnection:
         yield self.GetPoint(canvas, len(self.points) + 1)
     
     def GetPoint(self, canvas, index):
+        '''
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        '''
         if self.source is self.destination and len(self.points) == 0:
             topleft, bottomright = self.source.GetSquare(canvas)
             y = bottomright[1] + 30
@@ -344,6 +595,11 @@ class CConnection:
         return self.diagram
         
     def __ComputeIntersect(self, canvas, element, center, point):
+        '''
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        '''
         topLeft, bottomRight = element.GetSquare(canvas)
         square = CRectangle(CPoint(topLeft), CPoint(bottomRight))
         line = CLine(CPoint(center), CPoint(point))
@@ -363,21 +619,55 @@ class CConnection:
                 return point[0], bottomRight[1]
     
     def ValidatePoints(self, canvas):
+        '''
+        Remove unnecessary points from polyline forming connection and colapse
+        segments
+        
+        If two points are too close to each other, first of them is discarted.
+        If two segments contain angle too close to pi => create almost straight
+        line, middle point is discarted
+        
+        @param canvas: Canvas on which its being drawn
+        @type  canvas: L{CCairoCanvas<CCairoCanvas>}
+        '''
         points = list(self.GetPoints(canvas))
         lenold = len(points)
         changed = True
         for i in xrange(1, len(points) - 1):
-            pnts = [CPoint(pnt) for pnt in points[i-1:i+2]]
-            if pnts[0] - pnts[1] <= config['/Styles/Selection/PointsSize'] or \
-                pnts[1] - pnts[2] <= config['/Styles/Selection/PointsSize']:
+            if not self.ValidPoint(points[i-1:i+2]):
                 self.RemovePoint(canvas, i)
                 return
-            line1 = CLine(pnts[0], pnts[1])
-            line2 = CLine(pnts[1], pnts[2])
-            if -0.1 < line1.Angle() - line2.Angle() < 0.1:
-                self.RemovePoint(canvas, i)
-                return
+    
+    def ValidPoint(self, points):
+        '''
+        Check whether is middle point of the three at a valid position.
+        
+        Conditions for the middle point to be valid:
+        
+            - Middle point mustn't be too close to any of side points, closer
+            than /Styles/Selection/PointsSize  in config.xml
+            - Lines from 1st to 2nd and from 2nd to 3rd point must form angle 
+            sharper than (pi - /Styles/Connection/MinimalAngle)
+        
+        @param points: list of three points [(x1, y1), (x2, y2), (x3, y3)]. 
+        The middle point defined by (x2, y2) is to be examined
+        @type  points: list
+            
+        @return: True if both conditions stand
+        @rtype:  bool
+        '''
+        
+        pointSize = config['/Styles/Selection/PointsSize']
+        minAngle = config['/Styles/Connection/MinimalAngle']
+        
+        line1 = CLine(self.GetPoint(canvas, index), point)
+        line2 = CLine(point, self.GetPoint(canvas, index + 1))
+        
+        return ( abs(line1) > pointSize and abs(line2) > pointSize and
+            minAngle < (line1.Angle() - line.Angle()) % (2 * pi) < \
+            2 * pi - minAngle )
                 
     def RecalculateLabels(self):
         for id in self.labels:
             self.labels[id][0] = None
+
