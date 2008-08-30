e378a95badbba69c2953dc120c19c0a9e4b9c165
blueway <blueway@552e5214-defe-4a50-a20b-3e92271688c2>
2008-08-30 14:47:03 +0000

    Feature No. 0000081: Rename diagram is allowed

--- trunk/lib/Gui/txtNotes.py
+++ trunk/lib/Gui/txtNotes.py
@@ -1,5 +1,6 @@
 from common import CWidget, event
 from lib.Exceptions.UserException import *
+from lib.Drawing import CDiagram
 import gobject
 
 class CtxtNotes(CWidget):
@@ -21,6 +22,10 @@ class CtxtNotes(CWidget):
             self.txtNotes.get_buffer().set_text("")
             self.txtNotes.set_sensitive(False)
             return
+        
+        if isinstance(self.element, CDiagram):
+            return
+        
         object = Element.GetObject()
         type = Element.GetObject().GetType()
         cnt = 0
@@ -39,6 +44,8 @@ class CtxtNotes(CWidget):
     @event("txtNotes.buffer", "changed")
     def on_txtNotes_changed(self, buffer):
         if self.element is not None:
-            self.element.GetObject().SetAttribute(self.attr, buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter()))
-            
-            self.emit('content_update', self.element, self.attr)
+            if isinstance(self.element, CDiagram):
+                pass    #maybe, In the future, We can add notes to diagram
+            else:
+                self.element.GetObject().SetAttribute(self.attr, buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter()))
+                self.emit('content_update', self.element, self.attr)
