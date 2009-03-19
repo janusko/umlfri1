6f8a7c734da383916e3bfc78fb91b042cc723ccb
janik <jan.janech@kst.uniza.sk>
2009-03-19 18:37:57 +0000

    - frmException show only relative file paths (bug report #0000207) - frmException show application version too (bug report #0000205) - frmAbout shows system informations

--- lib/Gui/frmAbout.py
+++ lib/Gui/frmAbout.py
@@ -1,12 +1,31 @@
 from lib.Depend.gtk2 import pango
+import lib.Depend
 
 from common import CWindow, event
 from lib.config import config
 import lib.consts
 
 class CfrmAbout(CWindow):
-    widgets = ('tviewCredits', 'lblAboutUmlfri', 'lbtnProjectWeb', )
+    widgets = ('tviewCredits','tviewAboutSysInfo', 'lblAboutUmlfri', 'lbtnProjectWeb', )
     name = 'frmAbout'
+    
+    def __init__(self, app, wTree):
+        CWindow.__init__(self, app, wTree)
+
+        buff = self.tviewAboutSysInfo.get_buffer()  
+        tag_tab = buff.get_tag_table()
+
+        if tag_tab.lookup("bold") is None:
+            buff.create_tag("bold", weight=pango.WEIGHT_BOLD, family="monospace")
+        if tag_tab.lookup("mono") is None:
+            buff.create_tag("mono", family="monospace")
+        
+        iter = buff.get_iter_at_offset(0)
+        begin = ''
+        for name, version in lib.Depend.version():
+            buff.insert_with_tags_by_name(iter, begin+"%s:\t\t"%name, "bold")
+            buff.insert_with_tags_by_name(iter, version, "mono")
+            begin = '\n'
     
     def Show(self):
         # clear the buffer
--- lib/Gui/frmException.py
+++ lib/Gui/frmException.py
@@ -6,15 +6,17 @@ from common import CWindow
 from lib.config import config
 from lib.Gui.dialogs import CWarningDialog
 import sys, os, time, tarfile, traceback, cStringIO, datetime, urllib, urllib2
+import os.path
 
 EXCEPTION_PROJECT_FILE = 'error.frip'
 
 
 class CfrmException(CWindow):
-      
     widgets = ('tviewErrorLog','tviewSysInfo','btnCancel', 'btnSend',  'btnReport', 'ntbkException', 'lblMail', 'tviewUsrComment', 'chbtnIncludeProject',)
     name = 'frmException'
-
+    
+    __modPaths = [os.path.abspath(dir).replace(os.path.sep, "/") for dir in sys.path]
+    __modPaths.sort(key=len, reverse=True)
 
     def __init__(self, app, wTree):
         CWindow.__init__(self, app, wTree)
@@ -34,11 +36,11 @@ class CfrmException(CWindow):
             buff.create_tag("mono", family="monospace")
         
         iter = buff.get_iter_at_offset(0)
-        begin = ''
+        buff.insert_with_tags_by_name(iter, "UML .FRI:\t\t", "bold")
+        buff.insert_with_tags_by_name(iter, self.application.GetVersion(), "mono")
         for name, version in lib.Depend.version():
-            buff.insert_with_tags_by_name(iter, begin+"%s:\t\t"%name, "bold")
+            buff.insert_with_tags_by_name(iter, "\n%s:\t\t"%name, "bold")
             buff.insert_with_tags_by_name(iter, version, "mono")
-            begin = '\n'
     
     def OnChbtnIncludeProjectToogled(self, widget, event, data=None):
         if self.append_project == False:
@@ -167,6 +169,11 @@ class CfrmException(CWindow):
             buff.create_tag("mono", family="monospace")
 
         for filename, line_num, fun_name, text in traceback.extract_tb(tb)[1:]:
+            filename = os.path.abspath(filename).replace(os.path.sep, "/")
+            for dir in self.__modPaths:
+                if filename.startswith(dir):
+                    filename = filename[len(dir)+1:]
+                    break
             buff.insert_with_tags_by_name(iter, 'File ', "bold")
             buff.insert_with_tags_by_name(iter, filename,      "mono")
             buff.insert_with_tags_by_name(iter, ' line ',   "bold")
