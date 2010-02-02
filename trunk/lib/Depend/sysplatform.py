65cd4681b1370a59501446c54f95854abcb8e8a2
chabadat <chabadat@552e5214-defe-4a50-a20b-3e92271688c2>
2010-02-02 10:54:34 +0000

    bug #344 resolved - there was an difference between return type of method platform.python_version_tuple() according to version of python installed

--- trunk/lib/Depend/sysplatform.py
+++ trunk/lib/Depend/sysplatform.py
@@ -1,3 +1,5 @@
+from ctypes.wintypes import INT
+from xml.etree.ElementTree import tostring
 class platform:
     import sys as __sys
 
@@ -18,21 +20,24 @@ def check():
     
     req = [2, 5]
     ver = []
-    
-    for s in platform.python_version_tuple():
-        i = len(s)
-        while i:
-            try:
-                n = int(s[:i])
-                break
-            except ValueError:
-                i -= 1
-        if i:
-            if i < len(s):
-                n -= .5
-            ver.append(n)
-        else:
-            break
+    if type(platform.python_version_tuple()[0])==int:
+        for i in platform.python_version_tuple():
+            ver.append(i)
+    else:        
+        for s in platform.python_version_tuple():
+            i = len(s)
+            while i:
+                try:
+                    n = int(s[:i])
+                    break
+                except ValueError:
+                    i -= 1
+            if i:
+                if i < len(s):
+                    n -= .5
+                ver.append(n)
+            else:
+                break        
     
     if ver < req:
         raise AssertionError("You will need at least Python 2.5 to run UML .FRI")
