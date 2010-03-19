daee642de5c5aca6e07e25ff65fc000cdba6c045
janik <janik@552e5214-defe-4a50-a20b-3e92271688c2>
2010-03-19 11:58:05 +0000

    scripts connect.py and test.py are moved where they belongs to

--- trunk/share/tools/pluginDebug/connect.py
+++ trunk/share/tools/pluginDebug/connect.py
@@ -1,4 +1,8 @@
+import os.path
 import sys
+
+sys.path.append(os.path.abspath(os.path.join('..', '..', '..')))
+
 from lib.Addons.Plugin.Client.Interface import CInterface
 
 a = CInterface(int(sys.argv[1]))
