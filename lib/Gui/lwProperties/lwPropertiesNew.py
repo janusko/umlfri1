8260bb04233aa3ecc8d7fcded0021762c7f559b5
Peter Turza <peterturza@users.noreply.github.com>
2010-03-25 13:26:53 +0000

    Ref. 287 and 288 Font and color type for domains. Needs testing under different gtk environments. Tested under gtk 2.16.2

--- lib/Gui/lwProperties/lwPropertiesNew.py
+++ lib/Gui/lwProperties/lwPropertiesNew.py
@@ -7,7 +7,7 @@ from lib.Elements.Object import CElementObject
 from lib.Connections.Object import CConnectionObject
 from lib.Exceptions import *
 
-ID_ID, ID_NAME, ID_VALUE, ID_TEXT_VISIBLE, ID_COMBO_VISIBLE, ID_EDITABLE, ID_BUTTON_VISIBLE, ID_BUTTON_TEXT, ID_ACTION = range(9)
+ID_ID, ID_NAME, ID_VALUE, ID_TEXT_VISIBLE, ID_COMBO_VISIBLE, ID_EDITABLE, ID_BUTTON_VISIBLE, ID_BUTTON_TEXT, ID_ACTION, ID_COLOR,  ID_FONT = range(11)
 
 EDITABLE_COMBO_TYPES = ('int', 'float', 'text', 'str')
 
@@ -17,7 +17,7 @@ class ClwProperties(CWidget):
     
     def __init__(self, app, wTree):
         
-        self.treeStore = gtk.TreeStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_STRING, gobject.TYPE_STRING)
+        self.treeStore = gtk.TreeStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
         
         renderer = gtk.CellRendererText()
         self.combomodel = gtk.ListStore(gobject.TYPE_STRING)
@@ -56,6 +56,8 @@ class ClwProperties(CWidget):
         self.Column2.add_attribute(self.ButtonRenderer, 'visible', ID_BUTTON_VISIBLE)
         self.Column2.add_attribute(self.ButtonRenderer, 'text', ID_BUTTON_TEXT)
         self.Column2.add_attribute(self.ButtonRenderer, 'editable', ID_EDITABLE)
+        self.Column2.add_attribute(self.ButtonRenderer, 'color', ID_COLOR)
+        self.Column2.add_attribute(self.ButtonRenderer, 'font', ID_FONT)
         
         self.lwProperties.append_column(self.Column1)
         self.lwProperties.append_column(self.Column2)
@@ -130,9 +132,37 @@ class ClwProperties(CWidget):
                     ID_BUTTON_VISIBLE, True, 
                     ID_EDITABLE, False, #Change to True if has parser
                     ID_BUTTON_TEXT, 'Add item',
-                    ID_ACTION, 'listadd')
+                    ID_ACTION, 'listadd',
+                    ID_COLOR, "")
                 for idx, item in enumerate(object.GetValue(prefix + identifier)):
                     self._FillListItem(object, row, prefix + identifier, idx)
+            elif type=='color':
+                color = gtk.gdk.color_parse(str(object.GetValue(prefix + identifier)))
+                text = '('+str(int(round(color.red / 256))) + ', '+str(int(round(color.green / 256))) + ', '+str(int(round(color.blue / 256))) +')'
+                self.treeStore.set(row, 
+                    ID_ID, identifier,
+                    ID_NAME, name, 
+                    ID_VALUE, str(object.GetValue(prefix + identifier)), #text representation of color
+                    ID_TEXT_VISIBLE, False, 
+                    ID_COMBO_VISIBLE, False, 
+                    ID_BUTTON_VISIBLE, True, 
+                    ID_EDITABLE, False, #Change to True if has parser
+                    ID_BUTTON_TEXT, text,
+                    ID_ACTION, 'changecolor',
+                    ID_COLOR, str(object.GetValue(prefix + identifier)))
+            elif type=='font':
+                self.treeStore.set(row, 
+                    ID_ID, identifier,
+                    ID_NAME, name, 
+                    ID_VALUE, str(object.GetValue(prefix + identifier)), #text representation of font
+                    ID_TEXT_VISIBLE, False, 
+                    ID_COMBO_VISIBLE, False, 
+                    ID_BUTTON_VISIBLE, True, 
+                    ID_EDITABLE, False, #Change to True if has parser
+                    ID_BUTTON_TEXT, str(object.GetValue(prefix + identifier)),
+                    ID_ACTION, 'changefont',
+                    ID_FONT, str(object.GetValue(prefix + identifier)),
+                    ID_COLOR, "")
     
     def Fill(self, Element):
         self.element = Element
@@ -196,6 +226,47 @@ class ClwProperties(CWidget):
                 model.set(iter, ID_VALUE, str(self.element.GetObject().GetValue(key)))
                 raise ParserError(*e.params)
             self.application.GetBus().emit('content-update', self.element, key)
+            
+    def on_changecolor(self, key, iter):
+        cdia = gtk.ColorSelectionDialog("Select color")
+        colorsel = cdia.colorsel
+        if self.element.GetObject().GetValue(key) is not None:
+            colorsel.set_current_color(gtk.gdk.color_parse(self.element.GetObject().GetValue(key)))
+        response = cdia.run()
+        
+        if response == gtk.RESPONSE_OK:
+            colorsel = cdia.colorsel
+            color = colorsel.get_current_color()
+            model = self.lwProperties.get_model()
+            self.element.GetObject().SetValue(key, color.to_string())
+            model.set(iter, ID_VALUE, str(self.element.GetObject().GetValue(key)))
+            text = '('+str(int(round(color.red / 256))) + ', '+str(int(round(color.green / 256))) + ', '+str(int(round(color.blue / 256))) +')'
+            model.set(iter, ID_BUTTON_TEXT, text)
+            model.set(iter, ID_COLOR, self.element.GetObject().GetValue(key))
+        cdia.destroy()
+
+        self.application.GetBus().emit('content-update', self.element, key)
+        
+    def on_changefont(self, key, iter):
+        fdia = gtk.FontSelectionDialog("Select font")
+        if self.element.GetObject().GetValue(key) is not None:
+            fdia.set_font_name(self.element.GetObject().GetValue(key))
+        
+        response = fdia.run()
+        
+        if response == gtk.RESPONSE_OK:
+            
+            font_desc = fdia.get_font_name()
+
+            
+            model = self.lwProperties.get_model()
+            self.element.GetObject().SetValue(key, font_desc)
+            model.set(iter, ID_VALUE, str(self.element.GetObject().GetValue(key)))
+            model.set(iter, ID_BUTTON_TEXT, str(self.element.GetObject().GetValue(key)))
+            model.set(iter, ID_FONT, str(self.element.GetObject().GetValue(key)))
+        fdia.destroy()
+
+        self.application.GetBus().emit('content-update', self.element, key)
     
     def on_listadd(self, key, iter):
         self.element.GetObject().AppendItem(key)
--- lib/Gui/lwProperties/lwPropertiesOld.py
+++ lib/Gui/lwProperties/lwPropertiesOld.py
@@ -7,7 +7,7 @@ from lib.Elements.Object import CElementObject
 from lib.Connections.Object import CConnectionObject
 from lib.Exceptions import *
 
-ID_ID, ID_NAME, ID_VALUE, ID_TEXT_VISIBLE, ID_COMBO_VISIBLE, ID_EDITABLE, ID_BUTTON_VISIBLE, ID_MODEL, ID_BUTTON_TEXT, ID_ACTION = range(10)
+ID_ID, ID_NAME, ID_VALUE, ID_TEXT_VISIBLE, ID_COMBO_VISIBLE, ID_EDITABLE, ID_BUTTON_VISIBLE, ID_MODEL, ID_BUTTON_TEXT, ID_ACTION, ID_COLOR,  ID_FONT = range(12)
 
 EDITABLE_COMBO_TYPES = ('int', 'float', 'text', 'str')
 
@@ -17,7 +17,7 @@ class ClwProperties(CWidget):
     
     def __init__(self, app, wTree):
         
-        self.treeStore = gtk.TreeStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gtk.TreeModel, gobject.TYPE_STRING, gobject.TYPE_STRING)
+        self.treeStore = gtk.TreeStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN, gtk.TreeModel, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
         
         renderer = gtk.CellRendererText()
         self.Column1 = gtk.TreeViewColumn(_('Name'))
@@ -53,6 +53,8 @@ class ClwProperties(CWidget):
         self.Column2.add_attribute(self.ButtonRenderer, 'visible', ID_BUTTON_VISIBLE)
         self.Column2.add_attribute(self.ButtonRenderer, 'text', ID_BUTTON_TEXT)
         self.Column2.add_attribute(self.ButtonRenderer, 'editable', ID_EDITABLE)
+        self.Column2.add_attribute(self.ButtonRenderer, 'color', ID_COLOR)
+        self.Column2.add_attribute(self.ButtonRenderer, 'font', ID_FONT)
         
         self.lwProperties.append_column(self.Column1)
         self.lwProperties.append_column(self.Column2)
@@ -131,9 +133,37 @@ class ClwProperties(CWidget):
                     ID_BUTTON_VISIBLE, True, 
                     ID_EDITABLE, False, #Change to True if has parser
                     ID_BUTTON_TEXT, 'Add item',
-                    ID_ACTION, 'listadd')
+                    ID_ACTION, 'listadd',
+                    ID_COLOR, "")
                 for idx, item in enumerate(object.GetValue(prefix + identifier)):
                     self._FillListItem(object, row, prefix + identifier, idx)
+            elif type=='color':
+                color = gtk.gdk.color_parse(str(object.GetValue(prefix + identifier)))
+                text = '('+str(int(round(color.red / 256))) + ', '+str(int(round(color.green / 256))) + ', '+str(int(round(color.blue / 256))) +')'
+                self.treeStore.set(row, 
+                    ID_ID, identifier,
+                    ID_NAME, name, 
+                    ID_VALUE, str(object.GetValue(prefix + identifier)), #text representation of color
+                    ID_TEXT_VISIBLE, False, 
+                    ID_COMBO_VISIBLE, False, 
+                    ID_BUTTON_VISIBLE, True, 
+                    ID_EDITABLE, False, #Change to True if has parser
+                    ID_BUTTON_TEXT, text,
+                    ID_ACTION, 'changecolor',
+                    ID_COLOR, str(object.GetValue(prefix + identifier)))
+            elif type=='font':
+                self.treeStore.set(row, 
+                    ID_ID, identifier,
+                    ID_NAME, name, 
+                    ID_VALUE, str(object.GetValue(prefix + identifier)), #text representation of font
+                    ID_TEXT_VISIBLE, False, 
+                    ID_COMBO_VISIBLE, False, 
+                    ID_BUTTON_VISIBLE, True, 
+                    ID_EDITABLE, False, #Change to True if has parser
+                    ID_BUTTON_TEXT, str(object.GetValue(prefix + identifier)),
+                    ID_ACTION, 'changefont',
+                    ID_FONT, str(object.GetValue(prefix + identifier)),
+                    ID_COLOR, "")
     
     def Fill(self, Element):
         self.element = Element
@@ -178,7 +208,48 @@ class ClwProperties(CWidget):
             model.set(iter, ID_VALUE, str(self.element.GetObject().GetValue(key)))
             raise ParserError(*e.params)
         self.application.GetBus().emit('content-update', self.element, key)
-    
+        
+    def on_changecolor(self, key, iter):
+        cdia = gtk.ColorSelectionDialog("Select color")
+        colorsel = cdia.colorsel
+        if self.element.GetObject().GetValue(key) is not None:
+            colorsel.set_current_color(gtk.gdk.color_parse(self.element.GetObject().GetValue(key)))
+        response = cdia.run()
+        
+        if response == gtk.RESPONSE_OK:
+            colorsel = cdia.colorsel
+            color = colorsel.get_current_color()
+            model = self.lwProperties.get_model()
+            self.element.GetObject().SetValue(key, color.to_string())
+            model.set(iter, ID_VALUE, str(self.element.GetObject().GetValue(key)))
+            text = '('+str(int(round(color.red / 256))) + ', '+str(int(round(color.green / 256))) + ', '+str(int(round(color.blue / 256))) +')'
+            model.set(iter, ID_BUTTON_TEXT, text)
+            model.set(iter, ID_COLOR, self.element.GetObject().GetValue(key))
+        cdia.destroy()
+
+        self.application.GetBus().emit('content-update', self.element, key)
+        
+    def on_changefont(self, key, iter):
+        fdia = gtk.FontSelectionDialog("Select font")
+        if self.element.GetObject().GetValue(key) is not None:
+            fdia.set_font_name(self.element.GetObject().GetValue(key))
+        
+        response = fdia.run()
+        
+        if response == gtk.RESPONSE_OK:
+            
+            font_desc = fdia.get_font_name()
+
+            
+            model = self.lwProperties.get_model()
+            self.element.GetObject().SetValue(key, font_desc)
+            model.set(iter, ID_VALUE, str(self.element.GetObject().GetValue(key)))
+            model.set(iter, ID_BUTTON_TEXT, str(self.element.GetObject().GetValue(key)))
+            model.set(iter, ID_FONT, str(self.element.GetObject().GetValue(key)))
+        fdia.destroy()
+
+        self.application.GetBus().emit('content-update', self.element, key)
+        
     def on_listadd(self, key, iter):
         self.element.GetObject().AppendItem(key)
         self._FillListItem(self.element.GetObject(), iter, key, len(self.element.GetObject().GetValue(key)) - 1)
@@ -202,6 +273,8 @@ class ClwProperties(CWidget):
                 ID_NAME, str(idx))
         self.application.GetBus().emit('content-update', self.element, key)
     
+    
+    
     @event("ButtonRenderer", "click")
     def on_change_button(self, cellrenderer, path):
         model = self.lwProperties.get_model()
@@ -213,7 +286,12 @@ class ClwProperties(CWidget):
             
         elif action == 'listdel':
             self.on_listdel(key, iter, path)
+        elif action == 'changecolor':
+            self.on_changecolor(key, iter)
+        elif action == 'changefont':
+            self.on_changefont(key, iter)
         
+
     @event('application.bus', 'content-update-from-plugin')
     def on_content_update(self, widget, element, property):
         if (self.element is not None and (element is self.element 
