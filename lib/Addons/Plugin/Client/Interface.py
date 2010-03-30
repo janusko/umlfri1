66eb32fd2fd2dded9d6efde2cdf5288b2af13830
tomas <tomas.fri.zu@gmail.com>
2010-03-30 22:03:46 +0000

    It is possible to start gtk.main now in plugin. To do so, run interface.SetGtkMainloop() in constructor of plugin

--- lib/Addons/Plugin/Client/Interface.py
+++ lib/Addons/Plugin/Client/Interface.py
@@ -1,18 +1,32 @@
 from lib.Addons.Plugin.Client import classes
+from lib.Addons.Plugin.Client.Mainloop import CEmpty, CGtk
 from lib.Addons.Plugin.Communication.Connection import CConnection
 from Transaction import CTransaction
+from lib.Exceptions import *
 
 class CInterface(object):
     
     def __init__(self, port):
-        self.connection = CConnection(port)
+        self.mainloop = CEmpty()
+        self.inmainloop = False
+        self.connection = CConnection(port, self.mainloop)
         self.adapter = classes['IAdapter']('#adapter', self.connection)
     
     def _Init(self, uri):
         return self.connection.Execute('plugin', 'init', {'uri': uri})()
+        
+    def SetMainloop(self, mainloop):
+        if self.inmainloop:
+            raise PluginInMainloop('Mainloop object cannot be changed when another one already started')
+        self.mainloop = mainloop
+        self.connection.SetMainloop(mainloop)
     
-    def WaitTillClosed(self):
-        return self.connection.WaitTillClosed()
+    def SetGtkMainloop(self):
+        self.SetMainloop(CGtk())
+    
+    def Mainloop(self):
+        self.inmainloop = True
+        self.mainloop.Mainloop()
     
     def GetAdapter(self):
         return self.adapter
