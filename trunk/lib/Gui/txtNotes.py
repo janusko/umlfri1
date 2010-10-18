6c71d03dd5f5be820014f39cafd8f49fb50b74c4
tomas <tomas@552e5214-defe-4a50-a20b-3e92271688c2>
2010-10-18 12:58:20 +0000

    fixed problem with mixing CElement and CElementObject classes as parameter of content-update-from-plugin event

--- trunk/lib/Gui/txtNotes.py
+++ trunk/lib/Gui/txtNotes.py
@@ -10,8 +10,18 @@ class CtxtNotes(CWidget):
     widgets = ('txtNotes', )
     
     def __init__(self, app, wTree):
+        self.element = None
         CWidget.__init__(self, app, wTree)
         self.txtNotes.set_sensitive(False)
+    
+    @property
+    def elementObject(self):
+        if self.element is None:
+            return None
+        elif isinstance(self.element, (CElementObject, CConnectionObject, CDiagram)):
+            return self.element
+        else:
+            return self.element.GetObject()
     
     def Fill(self, Element):
         self.element = Element
@@ -20,9 +30,8 @@ class CtxtNotes(CWidget):
             self.txtNotes.set_sensitive(False)
             return
         
-        object = Element.GetObject()
-        if object.GetDomainType().HasAttribute('note'):
-            self.txtNotes.get_buffer().set_text(object.GetValue('note'))
+        if self.elementObject.GetDomainType().HasAttribute('note'):
+            self.txtNotes.get_buffer().set_text(self.elementObject.GetValue('note'))
             self.txtNotes.set_sensitive(True)
         else:
             self.txtNotes.get_buffer().set_text("")
@@ -31,8 +40,8 @@ class CtxtNotes(CWidget):
     @event("txtNotes.buffer", "changed")
     def on_txtNotes_changed(self, buffer):
         text = buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter())
-        if self.element is not None and self.element.GetObject().GetValue('note') != text:
-            self.element.GetObject().SetValue('note', text)
+        if self.element is not None and self.elementObject.GetValue('note') != text:
+            self.elementObject.SetValue('note', text)
             self.application.GetBus().emit('content-update', self.element, 'note')
                 
     @event('application.bus', 'content-update')
