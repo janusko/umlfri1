a1caaeb640e5f72ee923f6f91a0225504026d618
janik <jan.janech@kst.uniza.sk>
2010-12-21 17:24:10 +0000

    Exceptions can create inheritance hierarchy in public api definition

--- share/tools/mkaddonlib/lib/model/exception.py
+++ share/tools/mkaddonlib/lib/model/exception.py
@@ -1,11 +1,22 @@
 from baseContainer import BaseContainer
 
 class Exception(BaseContainer):
-    def __init__(self, name, namespace, documentation = None):
+    def __init__(self, name, namespace, base = None, documentation = None):
         BaseContainer.__init__(self, name, namespace)
         
         self.__documentation = documentation
+        self.__base = base
     
     @property
     def documentation(self):
         return self.__documentation
+    
+    @property
+    def base(self):
+        return self.__base
+    
+    def _link(self, builder):
+        BaseContainer._link(self, builder)
+        
+        if self.__base is not None:
+            self.__base = builder.getTypeByName(self.__base)
