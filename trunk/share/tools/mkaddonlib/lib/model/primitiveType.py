fa0f19c7a7ef787b28b1675924ded7f185d5227d
janik <janik@552e5214-defe-4a50-a20b-3e92271688c2>
2011-02-18 11:07:58 +0000

    Added missing float type

--- trunk/share/tools/mkaddonlib/lib/model/primitiveType.py
+++ trunk/share/tools/mkaddonlib/lib/model/primitiveType.py
@@ -47,6 +47,7 @@ primitiveTypes = {
     'boolean':      PrimitiveType('boolean', isLogic = True, convertor = bool, default = False),
     'inputstream':  PrimitiveType('inputstream', isObject = True),
     'int32':        PrimitiveType('int32', isNumeric = True, convertor = int, default = 0),
+    'float':        PrimitiveType('float', isNumeric = True, convertor = int, default = 0.0),
     'variant':      PrimitiveType('variant'),
     'string':       PrimitiveType('string', isString = True, convertor = str, default = ""),
     'xy':           PrimitiveType('xy', isObject = True),
