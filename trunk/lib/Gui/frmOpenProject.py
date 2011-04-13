3e7dba4bfeefa9f5d425a09f6c581332d4506ac2
janik <janik@552e5214-defe-4a50-a20b-3e92271688c2>
2011-04-13 17:56:52 +0000

    Native Open/Save dialogs on windows platform

--- trunk/lib/Gui/frmOpenProject.py
+++ trunk/lib/Gui/frmOpenProject.py
@@ -4,6 +4,8 @@ import common
 
 from lib.consts import PROJECT_EXTENSION, PROJECT_CLEARXML_EXTENSION, PROJECT_TPL_EXTENSION
 
+from win32 import COpenSaveDialog
+
 import os.path
 
 class CfrmOpenProject (common.CWindow):
@@ -12,31 +14,30 @@ class CfrmOpenProject (common.CWindow):
     glade = "project.glade"
 
     widgets = ("chkOpenAsCopy", )
-
+    
     def __init__ (self, app, wTree):
         common.CWindow.__init__ (self, app, wTree)
 
-        filter = gtk.FileFilter ()
-        filter.set_name (_("UML .FRI Projects"))
-        filter.add_pattern ("*"+PROJECT_EXTENSION)
-        self.form.add_filter (filter)
-
-        filter = gtk.FileFilter ()
-        filter.set_name (_("UML .FRI Clear XML Projects"))
-        filter.add_pattern ("*"+PROJECT_CLEARXML_EXTENSION)
-        self.form.add_filter (filter)
-
-        filter = gtk.FileFilter ()
-        filter.set_name (_("UML .FRI Projects templates"))
-        filter.add_pattern ("*"+PROJECT_TPL_EXTENSION)
-        self.form.add_filter (filter)
-
-        filter = gtk.FileFilter ()
-        filter.set_name (_("All files"))
-        filter.add_pattern ("*")
-        self.form.add_filter (filter)
+        self.filters = [
+            (_("UML .FRI Projects"), "*"+PROJECT_EXTENSION),
+            (_("UML .FRI Clear XML Projects"), "*"+PROJECT_CLEARXML_EXTENSION),
+            (_("UML .FRI Projects templates"), "*"+PROJECT_TPL_EXTENSION),
+        ]
+        
+        for text, pattern in self.filters:
+            filter = gtk.FileFilter ()
+            filter.set_name (text)
+            filter.add_pattern (pattern)
+            self.form.add_filter (filter)
 
     def ShowDialog (self, parent):
+        if COpenSaveDialog:
+            win = COpenSaveDialog(parent.form, 'open', self.form.get_title(), self.filters)
+            if win.ShowModal():
+                return win.GetAbsolutePath(), False
+            else:
+                return None, None
+        
         self.form.set_transient_for (parent.form)
         self.chkOpenAsCopy.set_active (False)
         try:
