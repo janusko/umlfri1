2661d13df71cc9f562006bd2398648a55e76bdde
jancura <matej.jancura@gmail.com>
2011-04-23 11:15:55 +0000

    Edit CAddon.py, CUpdate.py

--- lib/Update/CAddon.py
+++ lib/Update/CAddon.py
@@ -1,8 +1,10 @@
 class CAddon(object):
 	def __init__(self,url,uri,version=None):	#(url=string,uri=string,version=CVersion)
+		self.__uri = []
+		self.__version = []
 		self.__url = url
-		self.__uri = uri
-		self.__version = version
+		self.__uri.append(uri)
+		self.__version.append(version)
 	
 	def GetUrl(self):
 		return self.__url
@@ -12,3 +14,9 @@ class CAddon(object):
 
 	def GetVersion(self):
 		return self.__version
+	
+	def AppendUri(self,uri):
+		self.__uri.append(uri)
+	
+	def AppendVersion(self,version):
+		self.__version.append(version)
