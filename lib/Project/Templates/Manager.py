22ed5187a333a216cfc1e4bcb2414e2410673f9c
janik <jan.janech@kst.uniza.sk>
2011-10-09 15:58:25 +0000

    project can be created from project template using public api

--- lib/Project/Templates/Manager.py
+++ lib/Project/Templates/Manager.py
@@ -12,33 +12,52 @@ class CTemplateManager(CBaseObject):
     def __init__(self, addonManager):
         self.__dirs = (TEMPLATES_PATH, os.path.join(USERDIR_PATH, 'templates'))
         self.__addonManager = addonManager
+        self.__cache = {}
     
     def GetAllTemplates(self):
+        newCache = {}
         for dirname in self.__dirs:
             if os.path.exists(dirname):
                 for filename in os.listdir(dirname):
                     if not filename.endswith(PROJECT_TPL_EXTENSION):
                         continue
                     
-                    storage = open_storage(os.path.join(dirname, filename))
+                    filepath = os.path.join(dirname, filename)
                     
-                    if storage is None:
-                        continue
-                    
-                    if storage.exists('icon.png'):
-                        icon = 'icon.png'
+                    if filepath in self.__cache:
+                        tpl = self.__cache[filepath]
                     else:
-                        icon = None
+                        storage = open_storage(filepath)
+                        
+                        if storage is None:
+                            continue
+                        
+                        if storage.exists('icon.png'):
+                            icon = 'icon.png'
+                        else:
+                            icon = None
+                                                    
+                        tpl = CTemplate(filename.rsplit('.', 1)[0], storage, 'content.xml', icon)
                     
-                    yield CTemplate(filename.rsplit('.', 1)[0], storage, 'content.xml', icon)
+                    newCache[filepath] = tpl
+                    yield tpl
         
         for type in ('metamodel', 'composite'):
             for addon in self.__addonManager.ListEnabledAddons(type):
                 for name, icon, path in addon.GetComponent().GetTemplates():
-                    if name is None:
-                        name = path.replace('\\', '/').rsplit('/', 1)[-1].rsplit('.', 1)[0]
+                    key = addon.GetDefaultUri(), path
+                    if key in self.__cache:
+                        tpl = self.__cache[key]
+                    else:
+                        if name is None:
+                            name = path.replace('\\', '/').rsplit('/', 1)[-1].rsplit('.', 1)[0]
+                        
+                        if icon is None:
+                            icon = addon.GetIcon()
+                        
+                        tpl = CTemplate(name, addon.GetStorage(), path, icon, metamodelUri = addon.GetDefaultUri())
                     
-                    if icon is None:
-                        icon = addon.GetIcon()
-                    
-                    yield CTemplate(name, addon.GetStorage(), path, icon, metamodelUri = addon.GetDefaultUri())
+                    newCache[key] = tpl
+                    yield tpl
+        
+        self.__cache = newCache
