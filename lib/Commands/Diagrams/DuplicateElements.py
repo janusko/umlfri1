a8381f03d57ff885866124067381e9244d4ec4cb
harabin <jozef.hn@gmail.com>
2011-11-04 09:02:48 +0000

    minor fix in duplicating elements undo-redo

--- lib/Commands/Diagrams/DuplicateElements.py
+++ lib/Commands/Diagrams/DuplicateElements.py
@@ -7,24 +7,19 @@ from lib.Project import CProjectNode
 class CDuplicateElementsCommand(CCommand):
     def __init__(self, elements, diagram):
         CCommand.__init__(self)
-
         self.__originalElements = elements
         self.__duplicatedElements = []
         self.__diagram = diagram
+        self.__elementNodes = []
 
     def _Do(self):
         self.__diagram.DeselectAll()
+        # create element objects
         newElementObjects = []
-        try:
-            for element in self.__originalElements:
-                newElementobject = element.GetObject().Clone()
-                newElementObjects.append(newElementobject)
-        except UMLException, e:
-            for el in self.__duplicatedElements:
-                self.__diagram.DeleteElement(el)
-            raise
-
-        #recreate connections
+        for element in self.__originalElements:
+            newElementobject = element.GetObject().Clone()
+            newElementObjects.append(newElementobject)
+        # create coresponding connection objects
         mapping = dict(zip((x.GetObject() for x in self.__originalElements), newElementObjects))
         parsed = set()
         for element in (x.GetObject() for x in self.__originalElements):
@@ -41,32 +36,45 @@ class CDuplicateElementsCommand(CCommand):
                 src.AddConnection(newcon)
                 dest.AddConnection(newcon)
             parsed.add(mapping[element])
-
+        # create elements
         backward_mapping = dict(zip(newElementObjects, self.__originalElements))
-        for elobj in mapping.values():
-            newElement = Element.CElement(self.__diagram, elobj)
-            self.__diagram.AddToSelection(newElement)
-            element = backward_mapping[elobj]
-            newElement.CopyFromElement(element)
-            #shift element +(5, 5) units
-            newElement.SetPosition(map(sum, zip(newElement.GetPosition(), (5.0, 5.0))))
-            self.__duplicatedElements.append(newElement)
-
+        try:
+            for elobj in mapping.values():
+                newElement = Element.CElement(self.__diagram, elobj)
+                self.__diagram.AddToSelection(newElement)
+                element = backward_mapping[elobj]
+                newElement.CopyFromElement(element)
+                #shift element +(5, 5) units
+                newElement.SetPosition(map(sum, zip(newElement.GetPosition(), (5.0, 5.0))))
+                self.__duplicatedElements.append(newElement)
+        except UMLException, e:
+            for el in self.__duplicatedElements:
+                self.__diagram.DeleteElement(el)
+            raise
         self.__parentNode = self.__diagram.GetNode()
         for element in self.__duplicatedElements:
             elementNode = CProjectNode(self.__parentNode, element.GetObject())
             self.__parentNode.AddChild(elementNode)
+            self.__elementNodes.append(elementNode)
+        del self.__originalElements
 
     def _Redo(self):
+        self.__diagram.DeselectAll()
         for element in self.__duplicatedElements:
-            diagram.AddElement(element)
+            self.__diagram.AddElement(element)
+            self.__diagram.AddToSelection(element)
             elementNode = CProjectNode(self.__parentNode, element.GetObject())
-            self.__parentNode.Add(Child)
+            self.__parentNode.AddChild(elementNode)
+            self.__elementNodes.append(elementNode)
 
     def _Undo(self):
+        self.__diagram.DeselectAll()
         for element in self.__duplicatedElements:
             self.__diagram.DeleteElement(element)
-            self.__parentNode.RemoveChild(element.GetObject())
+        for elementNode in self.__elementNodes:
+            self.__parentNode.RemoveChild(elementNode)
+        self.__elementNodes = []
+
 
     def GetGuiUpdates(self):
         for element in self.__duplicatedElements:
