6eaf7fc7aa1ccb4e5f671172215a2d2f53e16016
Matej Melko <mmelko@users.noreply.github.com>
2012-04-12 09:38:25 +0000

    Added new slovak translations. In consts.py was disabled undo/redo system

--- share/tools/localegen/update_catalog.sh
+++ share/tools/localegen/update_catalog.sh
@@ -10,6 +10,6 @@ fi
 
 APP_PATH=${1:-../../..}
 PACKAGE_NAME='UML .FRI'
-PACKAGE_VERSION="$(grep __version__ $APP_PATH/main.py | cut -d"'" -f2)"
+PACKAGE_VERSION="$(grep '__version__ *=' $APP_PATH/main.py | cut -d"'" -f2)"
 
 find $APP_PATH | grep -E "(glade|py)$" | grep -v setup.py | xgettext -f- -o $APP_PATH/share/locale/uml_fri.pot -s --no-location --package-name="$PACKAGE_NAME" --package-version="$PACKAGE_VERSION"
