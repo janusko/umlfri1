7e757a2e6c1ede8aae6b06c5ecfd87d46c501ae2
janik <janik@552e5214-defe-4a50-a20b-3e92271688c2>
2014-04-26 19:37:17 +0000

    - plugin 2 tester is no longer part of the plugin system implementation - new plugin system can already start test plugin - new base for creating starters

--- branches/plugin2/lib/Addons/Plugin/AddonComponent.py
+++ branches/plugin2/lib/Addons/Plugin/AddonComponent.py
@@ -1,13 +1,26 @@
 from .PatchPlugin import CPatchPlugin
+from .Implementation.Plugin import CPlugin
+from .Starter import STARTERS
 
+import platform
+import os.path
 
 class CPluginAddonComponent(object):
-    def __init__(self, codes, patches, requiredMetamodels, patchParams):
+    __allowedOs = ('all', platform.system(), platform.system() + ' ' + platform.version())
+    
+    def __init__(self, codes, patches, patchParams):
+        self.__codes = codes
+        
         self.__addon = None
+        
         self.__patchPaths = patches
         self.__patchParams = patchParams
         self.__patches = None
         self.__patchStarted = False
+        
+        self.__plugin = None
+        self.__starter = None
+        self.__path = None
     
     def _SetAddon(self, addon):
         self.__addon = addon
@@ -27,12 +40,20 @@ class CPluginAddonComponent(object):
         for patch in self.__patches:
             patch.Start()
             self.__patchStarted = True
+        
+        path, starter = self.__GetStarter()
+        if path is not None:
+            channel = starter.Start()
+            self.__plugin = CPlugin(channel, self.__addon.GetManager().GetPluginAdapter())
     
     def Stop(self):
         if self.__patches is not None:
             for patch in self.__patches:
                 patch.Stop()
             self.__patchStarted = False
+        
+        if self.__plugin is not None:
+            self.__plugin.SendStop()
     
     def Terminate(self):
         if self.__patches is not None:
@@ -40,14 +61,38 @@ class CPluginAddonComponent(object):
                 patch.Stop()
             self.__patchStarted = False
         
+        if self.__plugin is not None:
+            self.__starter.Terminate()
+        
     def Kill(self):
         if self.__patches is not None:
             for patch in self.__patches:
                 patch.Stop()
             self.__patchStarted = False
         
+        if self.__plugin is not None:
+            self.__starter.Kill()
+        
     def IsRunning(self):
-        return self.__patchStarted
+        return self.__patchStarted or (self.__starter is not None and self.__starter.IsAlive())
     
     def GetRunInProcess(self):
-        return False
+        return self.__codes is not None
+    
+    def GetUri(self):
+        return self.__addon.GetDefaultUri()
+    
+    def GetPath(self):
+        return self.__path
+    
+    def __GetStarter(self):
+        root = self.__addon.GetStorage().get_path()
+        
+        if self.__codes is not None:
+            if self.__starter is None:
+                for osName, language, path in self.__codes:
+                    if osName in self.__allowedOs:
+                        self.__starter = STARTERS[language](self)
+                        self.__path = os.path.join(root, path)
+                        break
+        return self.__path, self.__starter
