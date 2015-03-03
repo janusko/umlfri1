4761e9b56892c6e5ede541b61502d2fe2a996860
louma <louma@st.fri.uniza.sk>
2015-03-03 22:53:56 +0000

    Fixed parameters for base class constructor call in CModifiedElementType.

--- lib/Elements/ModifiedType.py
+++ lib/Elements/ModifiedType.py
@@ -5,6 +5,6 @@ from Type import CElementType
 class CModifiedElementType(CElementType):
 
     def __init__(self, parentType, modifications):
-        super.__init__(parentType.GetName(), parentType.GetFactory())
+        super.__init__(parentType.GetFactory(), parentType.GetId())
         self.parentType = weakref.ref(parentType)
         self.modifications = modifications
