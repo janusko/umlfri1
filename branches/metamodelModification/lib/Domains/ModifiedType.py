aed625871be7248e32b46285ad4fcd55df1eea09
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-03-05 09:27:06 +0000

    Created method stubs to override in CModifiedDomainType.

--- branches/metamodelModification/lib/Domains/ModifiedType.py
+++ branches/metamodelModification/lib/Domains/ModifiedType.py
@@ -8,3 +8,15 @@ class CModifiedDomainType(CDomainType):
         CDomainType.__init__(parentType.GetName(), parentType.GetFactory())
         self.parentType = weakref.ref(parentType)
         self.modifications = modifications
+
+    def AppendAttribute(self, id, name, type = None, default = None, hidden=False):
+        CDomainType.AppendAttribute(id, name, type, default, hidden)
+
+    def HasAttribute(self, id):
+        return CDomainType.HasAttribute(self)
+
+    def GetAttribute(self, id):
+        return CDomainType.GetAttribute(id)
+
+    def IterAttributeIDs(self):
+        return CDomainType.IterAttributeIDs(self)
