252648da6fdf512d7e2fcb8aa54a616c9cf0c8a2
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-03-29 12:37:19 +0000

    Removed CModifiedElementFactory, since it isn't necessary. Reworked algorithm in CModificationTreeBuilder to properly encapsulate all element types, not just the modified ones.

--- branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationTreeBuilder.py
+++ branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationTreeBuilder.py
@@ -1,7 +1,6 @@
 from lib.Domains.ModifiedFactory import CModifiedDomainFactory
 from lib.Domains.ModifiedType import CModifiedDomainType
-from lib.Elements import CElementObject
-from lib.Elements.ModifiedFactory import CModifiedElementFactory
+from lib.Elements import CElementObject, CElementFactory, CElementAlias
 from lib.Elements.ModifiedType import CModifiedElementType
 from lib.Exceptions.DevException import MetamodelModificationError
 
@@ -16,8 +15,10 @@ class CModificationTreeBuilder:
     def BuildTree(self):
 
         # start with original element types from original metamodel
+        # element aliases are skipped, since they are not supported
         elementTypes = {type.GetId(): type
-                        for type in self.metamodel.GetElementFactory().IterTypes()}
+                        for type in self.metamodel.GetElementFactory().IterTypes()
+                        if not isinstance(type, CElementAlias)}
 
         elementTypeMappings = {}
 
@@ -36,14 +37,12 @@ class CModificationTreeBuilder:
                 if elementNode in self.elementModifications:
                     modifications = self.elementModifications[elementNode].GetElementTypeModifications()
                     modifiedElementType = self.__BuildTypeFromNode(elementTypes, elementNode, modifications)
-                    elementTypes = elementTypes[:]
-                    elementTypes[modifiedElementType.GetId()] = modifiedElementType
+                    elementTypes = dict(elementTypes)
 
                     elementTypeMappings[element] = modifiedElementType
                 else:
                     elementType = element.GetType()
-                    if elementType.GetId() in elementTypes:
-                        elementTypeMappings[element] = elementTypes[elementType.GetId()]
+                    elementTypeMappings[element] = elementTypes[elementType.GetId()]
 
             children = tuple((c, elementTypes) for c in self.__GetChildElements(elementNode))
             nodesToProcess.extend(children)
@@ -53,9 +52,8 @@ class CModificationTreeBuilder:
     @staticmethod
     def __GetChildElements(node):
         for c in node.GetChilds():
-            if c.GetObject() is CElementObject:
+            if isinstance(c.GetObject(), CElementObject):
                 yield c
-
 
     def __BuildTypeFromNode(self, elementTypes, elementNode, elementTypeModifications):
         factory = self.__BuildFactoryFromNode(elementTypes, elementNode, elementTypeModifications)
@@ -63,23 +61,45 @@ class CModificationTreeBuilder:
         return factory.GetElement(elementNode.GetObject().GetType().GetId())
 
     def __BuildFactoryFromNode(self, elementTypes, elementNode, elementTypeModifications):
-        modifiedElementFactory = CModifiedElementFactory(elementNode.GetObject().GetType().GetFactory())
+        modifiedElementFactory = CElementFactory(elementNode.GetObject().GetType().GetMetamodel())
 
-        for type, modifications in elementTypeModifications.iteritems():
-            if not elementTypes.has_key(type):
-                raise MetamodelModificationError('Creating new element types is not currently supported.')
+        # TODO: check if there are modifications for unknown types
+        # for type, modifications in elementTypeModifications.iteritems():
+        #     if not elementTypes.has_key(type):
+        #         raise MetamodelModificationError('Creating new element types is not currently supported.')
 
-            elementType = elementTypes[type]
+        # TODO: check if there are any modification for element aliases
+        # if isinstance(elementType, CElementAlias):
+        #     if elementTypeModifications.has_key()
+        #     raise MetamodelModificationError('Cannot modify element alias "%s"' % elementType.GetId())
 
-            factory = CModifiedDomainFactory(elementType.GetDomain().GetFactory())
-            self.__CreateModifiedDomainTypes(factory, elementType, modifications)
+        # similar algorithm as above for creating element object <-> type mappings
+        # - encapsulate types from elementTypes with new element factory
+        # - if there are modifications for given element type, stop and process them:
+        #   - create modified domain factory
+        #   - create modified domain types
+        # - replace types in elementTypes with the new, modified types
+
+        for id in elementTypes.keys():
+            elementType = elementTypes[id]
+
             modifiedElementType = CModifiedElementType(elementType, modifiedElementFactory)
-            modifiedElementType.SetDomain(factory.GetDomain(elementType.GetDomain().GetName()))
-            modifiedElementFactory.AddElement(modifiedElementType)
+            modifiedElementFactory.AddType(modifiedElementType)
+
+            domainfactory = elementType.GetDomain().GetFactory()
+            if elementTypeModifications.has_key(id):
+                modifications = elementTypeModifications[id]
+
+                domainfactory = CModifiedDomainFactory(elementType.GetDomain().GetFactory())
+                self.__CreateModifiedDomainTypes(domainfactory, modifications)
+
+            modifiedElementType.SetDomain(domainfactory.GetDomain(elementType.GetDomain().GetName()))
+
+            elementTypes[id] = modifiedElementType
 
         return modifiedElementFactory
 
     def __CreateModifiedDomainTypes(self, factory, domainModifications):
-        for name, modifications in domainModifications:
+        for name, modifications in domainModifications.iteritems():
             domain = CModifiedDomainType(factory.GetDomain(name), factory, modifications)
             factory.AddDomain(domain)
