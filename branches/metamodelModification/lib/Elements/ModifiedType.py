72742bc9b316fbac7f93edc6879e059af605c007
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-03-29 12:37:10 +0000

    Fixed calling base constructor in constructor of CModifiedElementType class.

--- branches/metamodelModification/lib/Elements/ModifiedType.py
+++ branches/metamodelModification/lib/Elements/ModifiedType.py
@@ -5,5 +5,5 @@ from Type import CElementType
 class CModifiedElementType(CElementType):
 
     def __init__(self, parentType, factory):
-        CElementType.__init__(factory, parentType.GetId())
+        CElementType.__init__(self, factory, parentType.GetId())
         self.parentType = weakref.ref(parentType)
