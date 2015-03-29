10723d49d32db310b4802745a2a38759d024b4a7
louma <louma@st.fri.uniza.sk>
2015-03-29 12:37:17 +0000

    CModifiedElementType now overwrites factory attribute in order to prevent from losing direct reference to its factory.

--- lib/Elements/ModifiedType.py
+++ lib/Elements/ModifiedType.py
@@ -5,5 +5,6 @@ from Type import CElementType
 class CModifiedElementType(CElementType):
 
     def __init__(self, parentType, factory):
-        CElementType.__init__(self, factory, parentType.GetId())
+        CElementType.__init__(self, None, parentType.GetId())
         self.parentType = weakref.ref(parentType)
+        self.factory = lambda: factory
