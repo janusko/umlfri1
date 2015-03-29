faa181b952c5b421e4caaea46e5b42ca9174705e
louma <louma@st.fri.uniza.sk>
2015-03-29 12:36:57 +0000

    Added Apply method to CMetamodelModification to help with debugging of metamodel modification builder code.

--- lib/Addons/Metamodel/Modifications/MetamodelModification.py
+++ lib/Addons/Metamodel/Modifications/MetamodelModification.py
@@ -8,3 +8,7 @@ class CMetamodelModification:
 
     def GetTypeForObject(self, object):
         return self.objectTypeMappings[object]
+
+    def Apply(self):
+        for object, type in self.objectTypeMappings.iteritems():
+            object.SetType(type)
