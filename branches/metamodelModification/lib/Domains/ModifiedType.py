eda97ee71bc7239a46b4f275989edf3d68a4b21d
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-03-29 12:41:16 +0000

    Added comments to extended __getattribute__ method for decorator/proxy pattern in CModifiedDomainType.

--- branches/metamodelModification/lib/Domains/ModifiedType.py
+++ branches/metamodelModification/lib/Domains/ModifiedType.py
@@ -33,23 +33,31 @@ class CModifiedDomainType(CDomainType):
         return attributes
 
     def __getattribute__(self, item):
+        # special cases, don't know how to address these properly
         if item == '__class__':
             return object.__getattribute__(self, item)
 
+        # attribute saved in instance (e.g. self.factory)
         if object.__getattribute__(self, '__dict__').has_key(item):
             return object.__getattribute__(self, item)
 
+        # attribute saved in class
         if self.__class__.__dict__.has_key(item):
             return object.__getattribute__(self, item)
 
+        # by now we should now that the attribute is not in this class or this instance,\
+        # so that remains only parent type
         if not hasattr(self.parentType(), item):
             raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__, item))
 
         obj = getattr(self.parentType(), item)
+
         if hasattr(obj, '__call__'):
+            # assume callable is bound method
             def proxy_method(*args, **kwargs):
                 return getattr(CDomainType, item)(self, *args, **kwargs)
 
             return proxy_method
         else:
+            # anything other than function from parent type is returned verbatim
             return obj
