1b52f5421af3e15f0e48a46076a305cd988027cd
louma <louma@st.fri.uniza.sk>
2015-03-29 12:37:21 +0000

    Finally implemented proper decorator/proxy hybrid in CModifiedElementType.

--- lib/Elements/ModifiedType.py
+++ lib/Elements/ModifiedType.py
@@ -5,6 +5,22 @@ from Type import CElementType
 class CModifiedElementType(CElementType):
 
     def __init__(self, parentType, factory):
-        CElementType.__init__(self, None, parentType.GetId())
         self.parentType = weakref.ref(parentType)
         self.factory = lambda: factory
+
+    def __getattribute__(self, item):
+        if object.__getattribute__(self, '__dict__').has_key(item):
+            return object.__getattribute__(self, item)
+
+        if not hasattr(self.parentType(), item):
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__, item))
+
+        obj = getattr(self.parentType(), item)
+        if hasattr(obj, '__call__'):
+            def proxy_method(*args, **kwargs):
+                return getattr(CElementType, item)(self, *args, **kwargs)
+
+            return proxy_method
+        else:
+            return obj
+
