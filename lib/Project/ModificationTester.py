22c2c6af62e911155a3a05f6292b1c94a1a56b07
louma <louma@st.fri.uniza.sk>
2015-03-29 12:41:09 +0000

    Changed ModificationTester to create modification on third child of root package (i.e. subpackage).

--- lib/Project/ModificationTester.py
+++ lib/Project/ModificationTester.py
@@ -2,8 +2,10 @@ def CreateModifications(project):
     if not project.GetRoot().HasChild():
         return
 
+    modificationRoot = list(project.GetRoot().GetChilds())[2]
+
     mmBuilder = project.CreateModification()
-    emBuilder = mmBuilder.CreateElementModifications(project.GetRoot())
+    emBuilder = mmBuilder.CreateElementModifications(modificationRoot)
     emBuilder.AddDomainAttribute('Class', 'class', 'docstring',
                                  dict(
                                      name='Documentation string',
