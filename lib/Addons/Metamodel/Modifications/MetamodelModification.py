14c5cc7642adb9250842808bba87546232cdd03d
louma <louma@st.fri.uniza.sk>
2015-03-29 12:33:52 +0000

    CMetamodelModification class is now only container for mappings of element object to its type.

--- lib/Addons/Metamodel/Modifications/MetamodelModification.py
+++ lib/Addons/Metamodel/Modifications/MetamodelModification.py
@@ -3,6 +3,8 @@ class CMetamodelModification:
     def __init__(self, objectTypeMappings):
         self.objectTypeMappings = objectTypeMappings
 
-    def Apply(self):
-        for type, element in self.objectTypeMappings:
-            element.SetType(type)
+    def __iter__(self):
+        return self.objectTypeMappings.iteritems()
+
+    def GetTypeForObject(self, object):
+        return self.objectTypeMappings[object]
