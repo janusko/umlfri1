6450c205668a38db265a4bfd01c4a8a8d67aea96
louma <louma@st.fri.uniza.sk>
2015-03-30 08:38:14 +0000

    Created again CModifiedElementFactory (in order to give parent factory proxy access to proper metamodel).

---
+++ lib/Elements/ModifiedFactory.py
@@ -0,0 +1,25 @@
+import weakref
+from lib.Elements import CElementFactory
+
+
+class CModifiedElementFactory(CElementFactory):
+    def __init__(self, parentFactory, metamodel):
+        self.parentFactory = weakref.ref(parentFactory)
+        self.metamodel = lambda: metamodel
+
+    def __getattribute__(self, item):
+        if object.__getattribute__(self, '__dict__').has_key(item):
+            return object.__getattribute__(self, item)
+
+        if not hasattr(self.parentFactory(), item):
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__, item))
+
+        obj = getattr(self.parentFactory(), item)
+        if hasattr(obj, '__call__'):
+            def proxy_method(*args, **kwargs):
+                return getattr(CModifiedElementFactory, item)(self, *args, **kwargs)
+
+            return proxy_method
+        else:
+            return obj
+
