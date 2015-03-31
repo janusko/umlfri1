74cfbc01ce82998d3d740c97a6a7f2936f0f9688
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-03-31 20:45:25 +0000

    Refactored dialog tab into separate class.

---
+++ branches/metamodelModification/lib/Gui/frmPropertiesWidgets/Abstract/DialogTab.py
@@ -0,0 +1,57 @@
+import gtk
+from lib.Gui.frmPropertiesWidgets import CEditableComboBox, CEditBoxWithButton, CEditBox, CComboBox, CTextArea, CTable
+
+
+class CDialogTab(object):
+    def __init__(self):
+        self.frame = gtk.Frame()
+        self.itemCount = 0
+
+        self.__vbox = gtk.VBox(False)
+        self.frame.add(self.__vbox)
+
+        self.__table = gtk.Table(0, 2)
+        self.__table.set_col_spacings(5)
+        self.__table.set_row_spacings(1)
+        self.__vbox.pack_start(self.__table, False, False, 0)
+
+        self.__vpaned = gtk.VPaned()
+        self.__vbox.pack_start(self.__vpaned)
+
+        self.frame.show_all()
+
+    def GetFrame(self):
+        return self.frame
+
+    def GetItemCount(self):
+        return self.itemCount
+
+    def AppendItem(self, item, itemname):
+        rows = self.GetItemCount()
+        self.itemCount += 1
+        if isinstance(item, CComboBox) or isinstance(item, CEditableComboBox) or \
+                isinstance(item, CEditBox) or isinstance(item, CEditBoxWithButton):
+            self.__table.resize(rows + 1, 2)
+            lbl = gtk.Label(itemname)
+            lbl.show()
+            align = gtk.Alignment(0, 0.5)
+            align.set_padding(0, 0, 5, 0)
+            align.show()
+            align.add(lbl)
+            self.__table.attach(align, 0, 1, rows, rows + 1, )
+            self.__table.attach((item.GetWidget()), 1, 2, rows, rows + 1)
+        elif isinstance(item, CTextArea):
+            if len(self.__vpaned.get_children()) < 1:
+                self.__vpaned.add1(item.GetWidget())
+            elif len(self.__vpaned.get_children()) < 2:
+                self.__vpaned.add2(item.GetWidget())
+            else:
+                self.__vbox.pack_start(item.GetWidget(), True, True)
+            item.GetWidget().set_label(itemname)
+        elif isinstance(item, CTable):
+            if len(self.__vpaned.get_children()) < 1:
+                self.__vpaned.add1(item.GetWidget())
+            elif len(self.__vpaned.get_children()) < 2:
+                self.__vpaned.add2(item.GetWidget())
+            else:
+                self.__vbox.pack_start(item.GetWidget(), True, True)
