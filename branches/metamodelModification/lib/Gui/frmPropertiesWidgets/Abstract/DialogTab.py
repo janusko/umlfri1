5b79cbdbb8dd275351eaa68b9f53a0024e9392f8
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-03-31 21:47:31 +0000

    Extracted table row item to separate class in CDialogTab.

--- branches/metamodelModification/lib/Gui/frmPropertiesWidgets/Abstract/DialogTab.py
+++ branches/metamodelModification/lib/Gui/frmPropertiesWidgets/Abstract/DialogTab.py
@@ -26,23 +26,27 @@ class CDialogTab(object):
     def GetItemCount(self):
         return len(self.items)
 
+    def RemoveItem(self, itemid):
+        if itemid not in self.items:
+            raise KeyError("Item with id {0} already exists".format(itemid))
+
+        item = self.items[itemid]
+
+        if isinstance(item, CComboBox) or isinstance(item, CEditableComboBox) or \
+                isinstance(item, CEditBox) or isinstance(item, CEditBoxWithButton):
+            self.__table.remove(item)
+
     def AppendItem(self, itemid, item, itemname):
         if itemid in self.items:
             raise KeyError("Item with id {0} already exists".format(itemid))
 
-        rows = self.GetItemCount()
+        # rows = self.GetItemCount()
         self.items[itemid] = item
         if isinstance(item, CComboBox) or isinstance(item, CEditableComboBox) or \
                 isinstance(item, CEditBox) or isinstance(item, CEditBoxWithButton):
-            self.__table.resize(rows + 1, 2)
-            lbl = gtk.Label(itemname)
-            lbl.show()
-            align = gtk.Alignment(0, 0.5)
-            align.set_padding(0, 0, 5, 0)
-            align.show()
-            align.add(lbl)
-            self.__table.attach(align, 0, 1, rows, rows + 1, )
-            self.__table.attach((item.GetWidget()), 1, 2, rows, rows + 1)
+            row_item =  CDialogTab.CTableRowItem(item, itemname)
+            self.items[itemid] = row_item
+            row_item.Append(self.__table)
         elif isinstance(item, CTextArea):
             if len(self.__vpaned.get_children()) < 1:
                 self.__vpaned.add1(item.GetWidget())
@@ -58,3 +62,27 @@ class CDialogTab(object):
                 self.__vpaned.add2(item.GetWidget())
             else:
                 self.__vbox.pack_start(item.GetWidget(), True, True)
+
+    class CTableRowItem(object):
+        def __init__(self, item, itemname):
+            self.item = item
+            lbl = gtk.Label(itemname)
+            lbl.show()
+            self.__align = gtk.Alignment(0, 0.5)
+            self.__align.set_padding(0, 0, 5, 0)
+            self.__align.show()
+            self.__align.add(lbl)
+
+        def Move(self, row, table):
+            self.Remove(table)
+            table.attach(self.__align, 0, 1, row, row + 1, )
+            table.attach((self.item.GetWidget()), 1, 2, row, row + 1)
+
+        def Append(self, table):
+            rows = len(table.get_children())
+            table.resize(rows + 1, 2)
+            self.Move(rows, table)
+
+        def Remove(self, table):
+            table.remove(self.__align)
+            table.remove(self.item.GetWidget())
