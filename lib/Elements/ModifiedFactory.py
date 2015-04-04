2c3e4a75a6755e5e09cb8a2c787b57a6bda85516
louma <louma@st.fri.uniza.sk>
2015-04-04 20:35:21 +0000

    Fixed __getattribute__ method in CModifiedElementFactory to properly create 'proxy' method objects for methods that are overriden in CModifiedElementFactory.

--- lib/Elements/ModifiedFactory.py
+++ lib/Elements/ModifiedFactory.py
@@ -1,3 +1,4 @@
+from types import MethodType
 import weakref
 from lib.Elements import CElementFactory
 
@@ -13,18 +14,34 @@ class CModifiedElementFactory(CElementFactory):
         return self.modifications
 
     def __getattribute__(self, item):
+        # special cases, don't know how to address these properly
+        if item == '__class__':
+            return object.__getattribute__(self, item)
+
+        # attribute saved in instance (e.g. self.factory)
         if object.__getattribute__(self, '__dict__').has_key(item):
             return object.__getattribute__(self, item)
 
-        if not hasattr(self.parentFactory(), item):
+        # attribute saved in class
+        if self.__class__.__dict__.has_key(item):
+            return object.__getattribute__(self, item)
+
+        parentFactory = self.parentFactory()
+
+        # by now we should now that the attribute is not in this class or this instance,\
+        # so that remains only parent type
+        if not hasattr(parentFactory, item):
             raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__, item))
 
-        obj = getattr(self.parentFactory(), item)
+        obj = getattr(parentFactory, item)
+
         if hasattr(obj, '__call__'):
-            def proxy_method(*args, **kwargs):
-                return getattr(CModifiedElementFactory, item)(self, *args, **kwargs)
-
-            return proxy_method
+            if isinstance(obj, weakref.ref):
+                return obj
+            elif isinstance(obj, MethodType):
+                return MethodType(obj.im_func, self)
+            else:
+                return obj
         else:
+            # anything other than function from parent type is returned verbatim
             return obj
-
