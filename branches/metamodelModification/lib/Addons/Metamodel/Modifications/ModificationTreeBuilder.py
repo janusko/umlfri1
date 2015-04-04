8b0335d75cbacfee852688bcf31ceba452446f60
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-04-04 17:25:14 +0000

    Refactored CModificationTreeBuilder to use CModifiedMetamodelBuilder.

--- branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationTreeBuilder.py
+++ branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationTreeBuilder.py
@@ -1,13 +1,12 @@
 from lib.Addons.Metamodel.Modifications.ElementObjectTypeMapping import CElementObjectTypeMapping
-from lib.Addons.Metamodel.ModifiedMetamodel import CModifiedMetamodel
-from lib.Domains.ModifiedFactory import CModifiedDomainFactory
-from lib.Domains.ModifiedType import CModifiedDomainType
-from lib.Elements import CElementObject, CElementFactory, CElementAlias
-from lib.Elements.ModifiedType import CModifiedElementType
+from lib.Addons.Metamodel.Modifications.ModifiedMetamodelBuilder import CModifiedMetamodelBuilder
+from lib.Elements import CElementObject, CElementAlias
 from lib.Exceptions.DevException import MetamodelModificationError
 
 
 class CModificationTreeBuilder:
+
+    __modifiedMetamodelBuilder = CModifiedMetamodelBuilder()
 
     def __init__(self, projectNode, elementModifications):
         self.projectNode = projectNode
@@ -24,12 +23,6 @@ class CModificationTreeBuilder:
             if isinstance(type, CElementAlias):
                 raise MetamodelModificationError('Cannot modify element alias "{0}"'.format(type))
 
-        # start with original element types from original metamodel
-        # element aliases are skipped, since they are not supported
-        elementTypes = {type.GetId(): type
-                        for type in factory.IterTypes()
-                        if not isinstance(type, CElementAlias)}
-
         elementTypeMappings = {}
 
         # algorithm overview:
@@ -39,21 +32,18 @@ class CModificationTreeBuilder:
         #     - for this project and its descendants, mappings will be created with this modified types
 
 
-        nodesToProcess = [(self.projectNode, elementTypes)]
+        nodesToProcess = [(self.projectNode, self.projectNode.GetMetamodel())]
         while len(nodesToProcess) > 0:
-            elementNode, elementTypes = nodesToProcess.pop(0)
+            elementNode, metamodel = nodesToProcess.pop(0)
             element = elementNode.GetObject()
+            elementTypeId = elementNode.GetType()
             if isinstance(element, CElementObject):
                 if elementNode == self.projectNode:
-                    modifiedElementType = self.__BuildTypeFromNode(elementTypes, elementNode, self.elementModifications)
-                    elementTypes = dict(elementTypes)
+                    metamodel = self.__modifiedMetamodelBuilder.BuildMetamodel(elementNode, self.elementModifications)
 
-                    elementTypeMappings[element] = CElementObjectTypeMapping(element, modifiedElementType)
-                else:
-                    elementType = element.GetType()
-                    elementTypeMappings[element] = CElementObjectTypeMapping(element, elementTypes[elementType.GetId()])
+                elementTypeMappings[element] = CElementObjectTypeMapping(element, metamodel.GetElementFactory().GetElement(elementTypeId))
 
-            children = tuple((c, elementTypes) for c in self.__GetChildElements(elementNode))
+            children = tuple((c, metamodel) for c in self.__GetChildElements(elementNode))
             nodesToProcess.extend(children)
 
         return elementTypeMappings
@@ -64,43 +54,7 @@ class CModificationTreeBuilder:
             if isinstance(c.GetObject(), CElementObject):
                 yield c
 
-    def __BuildTypeFromNode(self, elementTypes, elementNode, elementTypeModifications):
-        factory = self.__BuildFactoryFromNode(elementTypes, elementNode, elementTypeModifications)
+    def __BuildTypeFromNode(self, elementNode, elementTypeModifications):
+        factory = self.__modifiedMetamodelBuilder.BuildMetamodel(elementNode, elementTypeModifications)
 
         return factory.GetElement(elementNode.GetObject().GetType().GetId())
-
-    def __BuildFactoryFromNode(self, elementTypes, elementNode, elementTypeModifications):
-        modifiedMetamodel = CModifiedMetamodel(elementNode.GetObject().GetType().GetMetamodel(), elementNode, elementTypeModifications)
-
-        modifiedElementFactory = modifiedMetamodel.GetElementFactory()
-
-        # similar algorithm as above for creating element object <-> type mappings
-        # - encapsulate types from elementTypes with new element factory
-        # - if there are modifications for given element type, stop and process them:
-        #   - create modified domain factory
-        #   - create modified domain types
-        # - replace types in elementTypes with the new, modified types
-
-        for id in elementTypes.keys():
-            elementType = elementTypes[id]
-
-            modifiedElementType = CModifiedElementType(elementType, modifiedElementFactory)
-            modifiedElementFactory.AddType(modifiedElementType)
-
-            domainfactory = elementType.GetDomain().GetFactory()
-            if elementTypeModifications.has_key(id):
-                modifications = elementTypeModifications[id]
-
-                domainfactory = CModifiedDomainFactory(elementType.GetDomain().GetFactory())
-                self.__CreateModifiedDomainTypes(domainfactory, modifications)
-
-            modifiedElementType.SetDomain(domainfactory.GetDomain(elementType.GetDomain().GetName()))
-
-            elementTypes[id] = modifiedElementType
-
-        return modifiedElementFactory
-
-    def __CreateModifiedDomainTypes(self, factory, domainModifications):
-        for name, modifications in domainModifications.iteritems():
-            domain = CModifiedDomainType(factory.GetDomain(name), factory, modifications)
-            factory.AddDomain(domain)
