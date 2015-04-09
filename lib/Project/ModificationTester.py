79326e595dc22e27823d79287fded7590775de5e
louma <louma@st.fri.uniza.sk>
2015-04-09 21:57:46 +0000

    Updated ModificationTester to create attributes of various types (especially with enum values).

--- lib/Project/ModificationTester.py
+++ lib/Project/ModificationTester.py
@@ -1,5 +1,6 @@
 from lib.Addons.Metamodel.Modifications.ModificationBundleBuilder import CMetamodelModificationBundleBuidler
 from lib.Commands.Project.ApplyModificationBundles import CApplyModificationBundlesCommand
+from lib.Commands.Project.RemoveModificationBundles import CRemoveModificationBundlesCommand
 from lib.Domains.AttributeConditions import BuildParam
 
 
@@ -27,7 +28,8 @@ def CreateModifications(project):
                                      default=False
                                  ))
 
-    modification1 = (modificationRoot, mbBuilder.BuildBundles())
+    bundles1 = mbBuilder.BuildBundles()
+    modification1 = (modificationRoot, bundles1)
 
     modificationRoot = list(project.GetRoot().GetChilds())[3]
     mbBuilder = CMetamodelModificationBundleBuidler()
@@ -39,8 +41,36 @@ def CreateModifications(project):
                                      hidden=False,
                                      default=None
                                  ))
-    modification2 = (modificationRoot, mbBuilder.BuildBundles())
+    bundleBuilder.AddDomainAttribute('Class', 'class', 'metaclass',
+                                 dict(
+                                     name='Metaclass',
+                                     type='enum',
+                                     hidden=False,
+                                     default=None,
+                                     enum=['object', 'type', 'interface', 'Enum']
+                                 ))
+    bundleBuilder.AddDomainAttribute('Class', 'class', 'numbers',
+                                 dict(
+                                     name='Numbers',
+                                     type='int',
+                                     hidden=False,
+                                     default=5,
+                                     enum=[1, 2, 4, 5, 10]
+                                 ))
+    bundleBuilder.AddDomainAttribute('Class', 'class', 'coefficients',
+                                 dict(
+                                     name='Coefficients',
+                                     type='float',
+                                     hidden=False,
+                                     default=1.2,
+                                     enum=[0.8, 1.2, 2.2, 4.0, 15.44]
+                                 ))
+    bundles2 = mbBuilder.BuildBundles()
+    modification2 = (modificationRoot, bundles2)
 
     modifications = [modification1, modification2]
     for (root, bundles) in modifications:
         yield CApplyModificationBundlesCommand(root, bundles)
+    # yield CApplyModificationBundlesCommand(modificationRoot, merge_dicts(*[bundles for root, bundles in modifications]))
+    # yield CRemoveModificationBundlesCommand(modificationRoot, bundles2)
+
