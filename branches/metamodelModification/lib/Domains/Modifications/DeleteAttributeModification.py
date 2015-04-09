683fbdca559c1df5acea96d31789fc148679fcf7
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-04-09 21:57:43 +0000

    Added domain attribute modification type enum.

--- branches/metamodelModification/lib/Domains/Modifications/DeleteAttributeModification.py
+++ branches/metamodelModification/lib/Domains/Modifications/DeleteAttributeModification.py
@@ -1,7 +1,9 @@
-from DomainAttributeModification import CDomainAttributeModification
+from DomainAttributeModification import CDomainAttributeModification, DomainAttributeModificationType
 
 
 class CDeleteAttributeModification(CDomainAttributeModification):
+
+    type = DomainAttributeModificationType.DELETE
 
     def ApplyToAttributes(self, attributes):
         del attributes[self.attributeID]
