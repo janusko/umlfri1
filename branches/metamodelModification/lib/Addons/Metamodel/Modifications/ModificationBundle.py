66e1f1c8296f5ca3cf0e35e494b7bb6efa319b3d
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-04-13 16:01:05 +0000

    Renamed CElementModificationBuilder to CDomainModificationBuilder.

--- branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationBundle.py
+++ branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationBundle.py
@@ -1,11 +1,15 @@
 class CMetamodelModificationBundle(object):
 
-    def __init__(self, name, elementModifications):
+    def __init__(self, name, elementModifications, domainModifications):
         self.name = name
         self.elementModifications = elementModifications
+        self.domainModifications = domainModifications
 
     def GetName(self):
         return self.name
 
     def GetElementModifications(self):
         return self.elementModifications
+
+    def GetDomainModifications(self):
+        return self.domainModifications
