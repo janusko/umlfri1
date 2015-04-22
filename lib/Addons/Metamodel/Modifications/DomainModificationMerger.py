40bb9e5aef2a1199bf61ffda85609abfcc996415
louma <louma@st.fri.uniza.sk>
2015-04-22 19:29:41 +0000

    Extended CDomainModificationMerger with merging of two replace domain modifications, if type of both attributes is enum.

--- lib/Addons/Metamodel/Modifications/DomainModificationMerger.py
+++ lib/Addons/Metamodel/Modifications/DomainModificationMerger.py
@@ -1,4 +1,12 @@
+from lib.Domains.Modifications.DomainAttributeModification import DomainAttributeModificationType
+from lib.Domains.Modifications.ReplaceAttributeModification import CReplaceAttributeModification
+
+
 class CDomainModificationMerger(object):
+
+    def __init__(self, mergeEnumValues=True):
+        self.mergeEnumValues = mergeEnumValues
+
     def MergeModifications(self, parentModifications, childModifications):
         attributeModificationsResolver = lambda parent, child:  self.MergeAttributeModifications(parent, child)
         return self.__MergeObjects(parentModifications, childModifications, attributeModificationsResolver)
@@ -16,10 +24,39 @@ class CDomainModificationMerger(object):
 
         return mergedObjects
 
-    @staticmethod
-    def MergeAttributeModifications(parentModifications, childModifications):
+    def MergeAttributeModifications(self, parentModifications, childModifications):
         # TODO: optimize
         parentDict = {m.GetAttributeID(): m for m in parentModifications}
         childDict = {m.GetAttributeID(): m for m in childModifications}
-        merged = CDomainModificationMerger.__MergeObjects(parentDict, childDict, lambda parent, child: child)
+        merged = self.__MergeObjects(parentDict, childDict,
+                                     lambda parent, child: self.MergeAttributeModification(parent, child))
         return merged.values()
+
+    def MergeAttributeModification(self, parentModification, childModification):
+        # TODO: refactor, define merge rules, at least for replacements:
+        # e.g. enum -> enum (merge values), int -> int or float -> float (merge constraints)
+        # if needed, extend modification bundles (in API and builder too) with merge options
+
+        # don't merge, if both modifications are not replacements
+        if not (parentModification.GetType() == DomainAttributeModificationType.REPLACE) and (
+                    childModification.GetType() == DomainAttributeModificationType.REPLACE):
+
+            return childModification
+
+        parentProperties = parentModification.GetAttributeProperties()
+        childProperties = childModification.GetAttributeProperties()
+
+        # don't merge if both attributes are not enums
+        if parentProperties['type'] != 'enum' or childProperties['type'] != 'enum':
+            return childModification
+
+        # merge enum values
+        enumValues = set(parentProperties['enum'])
+        enumValues.update(childProperties['enum'])
+
+        # merge rest of attribute properties and use merged enum values
+        properties = dict(parentProperties)
+        properties.update(childProperties)
+        properties['enum'] = list(enumValues)
+        modification = CReplaceAttributeModification(parentModification.GetAttributeID(), properties)
+        return modification
