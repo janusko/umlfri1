83fec8719200cb3a572bd0a2f9de974262920ff0
louma <louma@st.fri.uniza.sk>
2015-04-23 16:29:54 +0000

    Updated __getattribute__ method in CModifiedElementType.

--- lib/Elements/ModifiedType.py
+++ lib/Elements/ModifiedType.py
@@ -1,3 +1,4 @@
+from types import MethodType
 import weakref
 from Type import CElementType
 
@@ -9,18 +10,34 @@ class CModifiedElementType(CElementType):
         self.factory = lambda: factory
 
     def __getattribute__(self, item):
+        # special cases, don't know how to address these properly
+        if item == '__class__':
+            return object.__getattribute__(self, item)
+
+        # attribute saved in instance (e.g. self.factory)
         if object.__getattribute__(self, '__dict__').has_key(item):
             return object.__getattribute__(self, item)
 
-        if not hasattr(self.parentType, item):
+        # attribute saved in class
+        if self.__class__.__dict__.has_key(item):
+            return object.__getattribute__(self, item)
+
+        parentType = self.parentType
+
+        # by now we should now that the attribute is not in this class or this instance,\
+        # so that remains only parent type
+        if not hasattr(parentType, item):
             raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__, item))
 
-        obj = getattr(self.parentType, item)
+        obj = getattr(parentType, item)
+
         if hasattr(obj, '__call__'):
-            def proxy_method(*args, **kwargs):
-                return getattr(CElementType, item)(self, *args, **kwargs)
-
-            return proxy_method
+            if isinstance(obj, weakref.ref):
+                return obj
+            elif isinstance(obj, MethodType):
+                return MethodType(obj.im_func, self)
+            else:
+                return obj
         else:
+            # anything other than function from parent type is returned verbatim
             return obj
-
