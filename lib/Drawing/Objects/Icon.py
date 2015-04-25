e82256517468e8039f0d659aa93486485b5303f3
louma <louma@st.fri.uniza.sk>
2015-04-25 22:50:50 +0000

    Icon visual object can be scaled via scale parameter.

--- lib/Drawing/Objects/Icon.py
+++ lib/Drawing/Objects/Icon.py
@@ -1,20 +1,30 @@
 from VisualObject import CVisualObject
 from ..IconSizeMeasurer import GetIconSize
 # import sys
+from lib.lib import ParseScale
+
 
 class CIcon(CVisualObject):
     types = {
-        'filename': str
+        'filename': str,
+        'scale': str
     }
-    def __init__(self, filename):
+    def __init__(self, filename, scale="1"):
         CVisualObject.__init__(self)
         self.filename = filename
+        self.scale = scale
 
     def ComputeSize(self, context):
-        filename, = self.GetVariables(context, 'filename')
-        return GetIconSize(context.GetMetamodel().GetStorage(), filename)
+        filename, scale = self.__GetParameters(context)
+        w, h = GetIconSize(context.GetMetamodel().GetStorage(), filename)
+        return w * scale, h * scale
 
     def Paint(self, context, canvas):
-        filename, = self.GetVariables(context, 'filename')
+        filename, scale = self.__GetParameters(context)
         
-        canvas.DrawIcon(context.GetPos(), filename)
+        canvas.DrawIcon(context.GetPos(), filename, scale)
+
+    def __GetParameters(self, context):
+        filename, scale = self.GetVariables(context, 'filename', 'scale')
+        scale = ParseScale(scale)
+        return filename, scale
