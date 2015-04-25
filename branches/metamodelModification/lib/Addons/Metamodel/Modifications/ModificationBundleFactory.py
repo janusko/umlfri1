419c50bff5b928b9223ee56fe5b035685696e74b
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-04-25 19:53:19 +0000

    Created separate factory for building domain attribute modifications from serialized form (list/dicts/etc.).

--- branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationBundleFactory.py
+++ branches/metamodelModification/lib/Addons/Metamodel/Modifications/ModificationBundleFactory.py
@@ -1,18 +1,8 @@
-from distutils.command.build import build
+from lib.Addons.Metamodel.Modifications.DomainModificationFactory import CDomainModificationFactory
 from lib.Addons.Metamodel.Modifications.ModificationBundleBuilder import CMetamodelModificationBundleBuilder
-from lib.Domains.AttributeConditions import BuildParam
 
-
-class AttributeModificationType:
-    Replace = 'replace'
-    Delete = 'delete'
 
 class CModificationBundleFactory(object):
-
-    defaultAttributeProperties = {
-        'default': None,
-        'hidden': False
-    }
 
     @classmethod
     def CreateFromList(cls, list):
@@ -21,24 +11,6 @@ class CModificationBundleFactory(object):
             bundleBuilder = builder.CreateBundle(bundleName)
 
             for domain, modifications in bundleDict.iteritems():
-                for m in modifications:
-                    attributeID =  m['attribute_id']
-                    modificationType = m['modification_type']
-                    if modificationType == AttributeModificationType.Delete:
-                        bundleBuilder.DeleteDomainAttribute(domain, attributeID)
-                    elif modificationType == AttributeModificationType.Replace:
-                        attributeProperties = m['attribute_properties']
-                        if 'condition' in attributeProperties:
-                            attributeProperties['condition'] = BuildParam(attributeProperties['condition'])
-
-                        cls.__SetDefaultAttributeProperties(attributeProperties)
-                        bundleBuilder.AddDomainAttribute(domain, attributeID, attributeProperties)
-                else:
-                    bundleBuilder.CreateDomain(domain)
+                CDomainModificationFactory.CreateFromList(domain, modifications, bundleBuilder)
 
         return builder.BuildBundles()
-
-    @classmethod
-    def __SetDefaultAttributeProperties(cls, props):
-        for name, value in cls.defaultAttributeProperties.iteritems():
-            props.setdefault(name, value)
