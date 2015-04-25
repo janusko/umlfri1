039216c5c78607ab032f1ae1020ce87e9741dc13
louma <louma@552e5214-defe-4a50-a20b-3e92271688c2>
2015-04-25 19:19:02 +0000

    Extended IProject interface to access attribute values of project's domain object.

--- branches/metamodelModification/lib/Addons/Plugin/Interface/Classes/Project.py
+++ branches/metamodelModification/lib/Addons/Plugin/Interface/Classes/Project.py
@@ -1,6 +1,8 @@
 from base import IBase
 from lib.Addons.Plugin.Communication.ComSpec import *
+from DomainObject import IDomainObject
 from lib.Addons.Plugin.Interface.decorators import *
+from lib.Commands.Project.SetProjectPropertyValues import CSetProjectPropertyValuesCommand
 from lib.Project.Project import CProject
 from lib.Exceptions import *
 from lib.Elements.Object import CElementObject
@@ -19,6 +21,18 @@ class IProject(IBase):
     
     def GetMetamodel(him):
         return him.GetMetamodel()
+
+
+    def GetValue(him, path):
+        return IDomainObject.GetValue(him.GetDomainObject(), path)
+
+    def GetAllValues(him):
+        return IDomainObject.GetAllValues(him.GetDomainObject())
+
+    @destructive
+    def SetValue(him, command, path, value):
+        cmd = CSetProjectPropertyValuesCommand(him, {path: value})
+        command.Execute(cmd)
     
     
     def Save(him):
