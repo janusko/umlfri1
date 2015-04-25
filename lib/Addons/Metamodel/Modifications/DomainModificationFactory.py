5b5a919c5aec4088c24c86d8fd4b25703e485710
louma <louma@st.fri.uniza.sk>
2015-04-25 19:53:19 +0000

    Created separate factory for building domain attribute modifications from serialized form (list/dicts/etc.).

---
+++ lib/Addons/Metamodel/Modifications/DomainModificationFactory.py
@@ -0,0 +1,41 @@
+from lib.Addons.Metamodel.Modifications.DomainModificationBuilder import CDomainModificationBuilder
+from lib.Domains.AttributeConditions import BuildParam
+
+class AttributeModificationType:
+    Replace = 'replace'
+    Delete = 'delete'
+
+
+class CDomainModificationFactory(object):
+
+    defaultAttributeProperties = {
+        'default': None,
+        'hidden': False
+    }
+
+    @classmethod
+    def CreateFromList(cls, domain, modifications, builder=None):
+        if builder is None:
+            builder = CDomainModificationBuilder()
+
+        for m in modifications:
+            attributeID =  m['attribute_id']
+            modificationType = m['modification_type']
+            if modificationType == AttributeModificationType.Delete:
+                builder.DeleteDomainAttribute(domain, attributeID)
+            elif modificationType == AttributeModificationType.Replace:
+                attributeProperties = m['attribute_properties']
+                if 'condition' in attributeProperties:
+                    attributeProperties['condition'] = BuildParam(attributeProperties['condition'])
+
+                cls.__SetDefaultAttributeProperties(attributeProperties)
+                builder.AddDomainAttribute(domain, attributeID, attributeProperties)
+        else:
+            builder.CreateDomain(domain)
+
+        return builder.GetDomainModifications()
+
+    @classmethod
+    def __SetDefaultAttributeProperties(cls, props):
+        for name, value in cls.defaultAttributeProperties.iteritems():
+            props.setdefault(name, value)
