0d3b323314a3c8fff0d279c16b8cc397fff93f8d
jurcisin <jurcisin@552e5214-defe-4a50-a20b-3e92271688c2>
2016-04-15 15:47:03 +0000

    Added NodeEvalWrapper and NodeTypeWrapper for '#node.children' expression. Added MetamodelError exception.

--- branches/oclmodul/lib/Drawing/Context/ParamEval.py
+++ branches/oclmodul/lib/Drawing/Context/ParamEval.py
@@ -1,29 +1,31 @@
 from ConfigEvalWrapper import CConfigEvalWrapper
-from NodeEvalWrapper import CNodeEvalWrapper
+from EvalWrapper import CEvalWrapper
 from grammar import parser as p
 from lib.Base import CBaseObject
-from lib.Drawing.Context.TypeWrappers import ObjectTypeWrapper, ConfigTypeWrapper, DomainTypeWrapper
+from lib.Drawing.Context.NodeEvalWrapper import NodeEvalWrapper
+from lib.Drawing.Context.TypeWrappers import ObjectTypeWrapper, ConfigTypeWrapper, DomainTypeWrapper, NodeTypeWrapper
+from lib.Exceptions.UserException import MetamodelError
 
 
 class CParamEval(CBaseObject):
     def __init__(self, str_, domainType, localvars, type_=None):
         self.__str = str_
         self.__type = type_
-        evaltype = None
         try:
             self.__ast = p.parse(str_)
             vars_ = dict(
                 self=DomainTypeWrapper(domainType),
+                node=NodeTypeWrapper(),
                 cfg=ConfigTypeWrapper(),
-                _line=None
+                _line=int
             )
             vars_.update(localvars)
             evaltype = p.checktype2(self.__ast, ObjectTypeWrapper(), vars_)
         except Exception as e:
             if type_ is not None:
                 type_ = type_.__name__
-            print "Exception: {0}, STR: {1}, TYPE: {2}".format(e, str_, type_)
-            return
+            raise MetamodelError("Error during type checking: {0}. Checked string: '{1}', expected type: '{2}'."
+                            .format(e, str_, type_))
 
         if evaltype in (list, str) and type_ == bool:
             pass
@@ -32,22 +34,21 @@ class CParamEval(CBaseObject):
         elif type_ is None:
             pass
         elif evaltype != type_:
-            #raise TypeError("Element attribute in metamodel have bad type: {0}, {1}".format(evaltype, type_))
             if evaltype is not None:
                 evaltype = evaltype.__name__
             if type_ is not None:
                 type_ = type_.__name__
-            print "Error: {0} != {1}    ON: {2}".format(evaltype, type_, str_)
+            raise MetamodelError("Different types: domain type: {0} != expected type {1}, checked string: {2}".format(evaltype, type_, str_))
+            #print "Error: {0} != {1}    ON: {2}".format(evaltype, type_, str_)
 
     def __call__(self, context):
         locals = dict(
-            self = CNodeEvalWrapper(context.GetDomainObject(), context.GetProjectNode()),
+            self = CEvalWrapper(context.GetDomainObject()),
+            node = NodeEvalWrapper(context.GetDomainObject(), context.GetProjectNode()),
             cfg = CConfigEvalWrapper(),
             _line = context.GetLine(),
         )
         locals.update(context.GetVariables())
-        #value = eval(self.__code, locals, {'__builtins__': {}})
-        #value = p.evalexp(self.__exp, locals)
         value = p.evalexp2(self.__ast, locals)
 
         if self.__type is not None:
